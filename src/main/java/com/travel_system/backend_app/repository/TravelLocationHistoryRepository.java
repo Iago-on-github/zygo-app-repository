7350a3ac218b59ccb789e0e7ab8deccfb3515f16
Iago device note <Iago737@outlook.com>
2026-03-19 22:01:47 -0300

    feat: - criaçao de query que retorna os dados de lat/lng da viagem ordenados pelo timestamp e com algoritmo ASC

--- src/main/java/com/travel_system/backend_app/repository/TravelLocationHistoryRepository.java
+++ src/main/java/com/travel_system/backend_app/repository/TravelLocationHistoryRepository.java
@@ -1,11 +1,14 @@
 package com.travel_system.backend_app.repository;
 
 import com.travel_system.backend_app.model.TravelLocationHistory;
+import com.travel_system.backend_app.model.dtos.route.LocationPointDTO;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -13,4 +16,8 @@ public interface TravelLocationHistoryRepository extends JpaRepository<TravelLoc
     List<TravelLocationHistory> findAllByTravelIdOrderByTimestampAsc(UUID travelId);
 
     void deleteAllByTravelId(UUID travelId);
+
+    @Query("SELECT new com.travel_system.backend_app.model.dtos.route.LocationPointDTO(t.latitude, t.longitude) " +
+            "FROM TravelLocationHistory t WHERE t.travelId = :travelId ORDER BY t.timestamp ASC")
+    List<LocationPointDTO> findLatLongByTravelIdAsc(UUID travelId, Pageable pageable);
 }
