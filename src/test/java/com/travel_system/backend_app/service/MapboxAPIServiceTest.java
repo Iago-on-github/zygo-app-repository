7bda4215291a40f7d5379d07b450fec5d2e06690
Iago device note <Iago737@outlook.com>
2026-04-04 22:13:16 -0300

    tests: - criação de classe de teste para o service MapboxAPIService.

---
+++ src/test/java/com/travel_system/backend_app/service/MapboxAPIServiceTest.java
@@ -0,0 +1,47 @@
+package com.travel_system.backend_app.service;
+
+import com.travel_system.backend_app.repository.TravelRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class MapboxAPIServiceTest {
+    /*
+     * PADRÕES DOS TESTES UNITÁRIOS
+     * 1. TESTAR TODOS AS SAÍDAS (RESULTADOS) DOS MÉTODOS EM QUESTÃO
+     * 2. OS MÉTODOS SUCCESS DEVEM CONTER "WithSuccess"
+     * 3. OS MÉTODOS FAILURE DEVEM CONTER "ThrowException"
+     * 4. MÉTODOS COM VÁRIAS POSSÍVEIS SAÍDAS DEVEM SER OBRIGATORIAMENTE ENGLOBADAS EM CLASSES PRÓPRIAS DE SUCCESS E FAILURE (MESMO DENTRO DA SUA CLASSE DE ORIGEM)
+     * 5. NUNCA USAR RUNTIME EX. COMO EXCEÇÃO CORINGA, USE A PRÓPRIA EXCEÇÃO LANÇADA NO MÉTODO
+     * 6. SEMPRE ADICIONAR UMA BREVE DESCRIÇÃO COM A ANNOTATION '@DisplayName("...")'.
+     * 7. OS TESTES DEVEM OBRIGATORIAMENTE SEGUIR O PADRÃO AAA (ARRANGE, ACT & ASSERT)
+     */
+
+    @InjectMocks
+    private MapboxAPIService mapboxAPIService;
+
+    @Mock
+    private WebClient webClient;
+    @Mock
+    private TravelRepository travelRepository;
+
+    private String accessToken;
+
+    @Nested
+    class calculateRoute {
+
+        @Test
+        @DisplayName("should return All API data with success")
+        void shouldReturnAllApiDataWithSuccess() {
+
+        }
+    }
+}
