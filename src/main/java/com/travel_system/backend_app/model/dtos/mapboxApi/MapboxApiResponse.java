8f163d1b093eeed53742b7fb459ed81c79687605
Iago device note <Iago737@outlook.com>
2026-04-05 22:13:59 -0300

    feat: - adião de jsonIgoreProperties e jsonproperty para melhor mapeamento nos testes

--- src/main/java/com/travel_system/backend_app/model/dtos/mapboxApi/MapboxApiResponse.java
+++ src/main/java/com/travel_system/backend_app/model/dtos/mapboxApi/MapboxApiResponse.java
@@ -1,10 +1,14 @@
 package com.travel_system.backend_app.model.dtos.mapboxApi;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record MapboxApiResponse(
-        List<RoutesDTO> routes,
-        List<WaypointsDTO> waypoints,
-        String code,
-        String uuid
+        @JsonProperty("routes") List<RoutesDTO> routes,
+        @JsonProperty("waypoints") List<WaypointsDTO> waypoints,
+        @JsonProperty("code") String code,
+        @JsonProperty("uuid") String uuid
 ) {}
