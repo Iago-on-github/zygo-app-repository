73d32149107495f65e32866bb40cd7a482f649c8
Iago device note <Iago737@outlook.com>
2026-04-07 22:21:14 -0300

    fix: - removido lançameto de exception indevida dos métodos desse serviço - adicionado retorno de null + logger quando polylineRoute for empty

--- src/main/java/com/travel_system/backend_app/service/PolylineService.java
+++ src/main/java/com/travel_system/backend_app/service/PolylineService.java
@@ -3,6 +3,8 @@ package com.travel_system.backend_app.service;
 import com.mapbox.geojson.Point;
 import com.mapbox.geojson.utils.PolylineUtils;
 import com.travel_system.backend_app.exceptions.NoSuchCoordinates;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,11 +12,14 @@ import java.util.List;
 @Service
 public class PolylineService {
 
+    private Logger logger = LoggerFactory.getLogger(PolylineService.class);
+
     public List<Point> formattedPolylineDecoded(String polylineRoute) {
         int precision = 5;
 
         if (polylineRoute.isEmpty()) {
-            throw new NoSuchCoordinates("Polyline está vazio, " + polylineRoute);
+            logger.debug("[formattedPolylineDecoded] Polyline recebido está vazio: {} ", polylineRoute);
+            return null;
         }
 
         return PolylineUtils.decode(polylineRoute, precision);
@@ -24,7 +29,8 @@ public class PolylineService {
         int precision = 5;
 
         if (polylineRoute.isEmpty()) {
-            throw new NoSuchCoordinates("Polyline está vazio, " + polylineRoute);
+            logger.debug("[formattedPolylineEncoded] Polyline recebido está vazio: {} ", polylineRoute);
+            return null;
         }
 
         return PolylineUtils.encode(polylineRoute, precision);
