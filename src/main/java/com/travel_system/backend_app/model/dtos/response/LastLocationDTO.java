028152817ea5e5fa9a90b1852ad76fe8ecfeff9b
Iago device note <Iago737@outlook.com>
2026-04-23 20:09:30 -0300

    fix: - mudança do tipo de dado do dto, para lidar com valores null

--- src/main/java/com/travel_system/backend_app/model/dtos/response/LastLocationDTO.java
+++ src/main/java/com/travel_system/backend_app/model/dtos/response/LastLocationDTO.java
@@ -2,7 +2,7 @@ package com.travel_system.backend_app.model.dtos.response;
 
 import java.time.Instant;
 
-public record LastLocationDTO(double latitude,
-                              double longitude,
-                              long timestamp) {
+public record LastLocationDTO(Double latitude,
+                              Double longitude,
+                              Long timestamp) {
 }
