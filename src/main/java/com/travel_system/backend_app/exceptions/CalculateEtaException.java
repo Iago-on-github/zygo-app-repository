9162ca7b9a9199bd757d9bf043488e5e3fbd87ff
Iago device note <Iago737@outlook.com>
2026-05-04 22:12:00 -0300

    feat: - crição de nova exception para o ecossistema do mapbox

---
+++ src/main/java/com/travel_system/backend_app/exceptions/CalculateEtaException.java
@@ -0,0 +1,7 @@
+package com.travel_system.backend_app.exceptions;
+
+public class CalculateEtaException extends RuntimeException {
+    public CalculateEtaException(String message) {
+        super(message);
+    }
+}
