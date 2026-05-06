f5d5f343ec15ca22be9e1f1619b60a7747c8dd6f
Iago-on-github <Iago737@outlook.com>
2026-05-06 11:10:01 -0300

    tests IT: - criação da classe TravelTrackingControllerIT para os it tests

---
+++ src/test/java/com/travel_system/backend_app/integration/controller/TravelTrackingControllerIT.java
@@ -0,0 +1,14 @@
+package com.travel_system.backend_app.integration.controller;
+
+import com.travel_system.backend_app.integration.IntegrationTestBase;
+import org.junit.jupiter.api.Nested;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TravelTrackingControllerIT extends IntegrationTestBase {
+
+    @Nested
+    class markDriverCheckpoint {
+
+    }
+}
