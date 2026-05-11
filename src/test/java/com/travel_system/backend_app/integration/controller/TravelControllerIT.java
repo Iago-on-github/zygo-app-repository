769413767a2b115f66130b6922f9764515e78048
Iago-on-github <Iago737@outlook.com>
2026-05-11 20:56:16 -0300

    IT tests: - implementação de testes para o startTravel.

--- src/test/java/com/travel_system/backend_app/integration/controller/TravelControllerIT.java
+++ src/test/java/com/travel_system/backend_app/integration/controller/TravelControllerIT.java
@@ -5,6 +5,7 @@ import com.travel_system.backend_app.model.City;
 import com.travel_system.backend_app.model.Driver;
 import com.travel_system.backend_app.model.Permissions;
 import com.travel_system.backend_app.model.Travel;
+import com.travel_system.backend_app.model.dtos.mapboxApi.RouteDetailsDTO;
 import com.travel_system.backend_app.model.dtos.request.TravelRequestDTO;
 import com.travel_system.backend_app.model.dtos.request.VehicleLocationRequestDTO;
 import com.travel_system.backend_app.model.enums.CitySize;
@@ -18,16 +19,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.http.MediaType;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -85,16 +94,6 @@ public class TravelControllerIT extends IntegrationTestBase {
                     "Salvador", 0, new ArrayList<>());
             driverRepository.save(driver);
 
-//            travel = new Travel(
-//                    null, null, TravelStatus.TRAVELLING, driver,
-//                    Instant.now(), null, "~shnC~_rcL_@v@m@p@y@r@",
-//                    3600.0, 15000.0,
-//                    -12.9714, -38.5016,
-//                    -12.8000, -38.4000
-//            );
-//            travelRepository.save(travel);
-//            travel.setStudentTravels(new HashSet<>());
-
             travelRequestDTO = new TravelRequestDTO(driver.getId(), -38.501200, -12.971800, -38.482300, -12.950400);
         }
 
@@ -151,4 +150,114 @@ public class TravelControllerIT extends IntegrationTestBase {
             assertTrue(result);
         }
     }
+
+    @Nested
+    class startTravel {
+        Driver driver;
+        TravelRequestDTO travelRequestDTO;
+        Travel travel;
+
+        @BeforeEach
+        void setUp() {
+            driver = new Driver(
+                    null, "driver@test.com", "encoded_pass",
+                    "João", "Silva", "71999999999",
+                    null, GeneralStatus.ACTIVE,
+                    LocalDateTime.now(), LocalDateTime.now(),
+                    "Salvador", 0, new ArrayList<>());
+            driverRepository.save(driver);
+
+             travel = new Travel(
+                    null, null, TravelStatus.PENDING, driver,
+                    Instant.now(), null, "~shnC~_rcL_@v@m@p@y@r@",
+                    3600.0, 15000.0,
+                    -12.9714, -38.5016,
+                    -12.8000, -38.4000
+            );
+            travelRepository.save(travel);
+
+            travelRequestDTO = new TravelRequestDTO(driver.getId(), -38.501200, -12.971800, -38.482300, -12.950400);
+        }
+
+        @Test
+        void shouldStartTravelWithSuccess() throws Exception {
+            when(mapboxAPIService.calculateRoute(anyDouble(), anyDouble(), anyDouble(), anyDouble()))
+                    .thenReturn(new RouteDetailsDTO(35.3, 2034.3, "encoded_geometry_response"));
+
+            mockMvc.perform(post("/travel/start/{travelId}", travel.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(travelRequestDTO)))
+                    .andDo(print())
+                    .andExpect(status().isNoContent());
+
+            Travel result = travelRepository.findById(travel.getId()).orElseThrow();
+
+            assertEquals(TravelStatus.TRAVELLING, result.getTravelStatus());
+            assertEquals(35.3, result.getDuration());
+            assertEquals("encoded_geometry_response", result.getPolylineRoute());
+            assertNotNull(result.getStartHourTravel());
+        }
+
+        @Test
+        @DisplayName("should persist route data for self-health of the system")
+        void shouldPersistRouteDataWithSuccess() throws Exception {
+            String key = "ACTIVE_TRAVELS_KEY";
+
+            when(mapboxAPIService.calculateRoute(anyDouble(), anyDouble(), anyDouble(), anyDouble()))
+                    .thenReturn(new RouteDetailsDTO(07.2, 304.2, "encoded_geometry"));
+
+            mockMvc.perform(post("/travel/start/{travelId}", travel.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(travelRequestDTO)))
+                    .andExpect(status().isNoContent());
+
+            SetOperations<String, String> hashSet = redisTemplate.opsForSet();
+
+            assertTrue(redisTemplate.opsForSet().members(key).contains(travel.getId().toString()));
+            assertEquals(1, hashSet.size(key));
+        }
+
+        @ParameterizedTest
+        @MethodSource("travelStatusProvider")
+        void throwExceptionWhenTravelIsNotTravelling(TravelStatus travelStatus) throws Exception {
+            travel.setTravelStatus(travelStatus);
+            travelRepository.save(travel);
+
+            mockMvc.perform(post("/travel/start/{travelId}", travel.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(travelRequestDTO)))
+                    .andExpect(status().isConflict());
+        }
+
+        public static Stream<Arguments> travelStatusProvider() {
+            return Stream.of(
+                    Arguments.of(TravelStatus.TRAVELLING),
+                    Arguments.of(TravelStatus.FINISH)
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("routeDetailsProvider")
+        void throwExceptionWhenTheMapboxApiReturnsInvalidResponse(RouteDetailsDTO routeDetailsDTO) throws Exception {
+            when(mapboxAPIService.calculateRoute(anyDouble(), anyDouble(), anyDouble(), anyDouble()))
+                    .thenReturn(routeDetailsDTO);
+
+            mockMvc.perform(post("/travel/start/{travelId}", travel.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(travelRequestDTO)))
+                    .andExpect(status().isBadGateway());
+        }
+
+        public static Stream<Arguments> routeDetailsProvider() {
+            return Stream.of(
+                    Arguments.of(new RouteDetailsDTO(null, 300.3, "encoded_geometry")),
+                    Arguments.of(new RouteDetailsDTO(07.2, null, "encoded_geometry")),
+                    Arguments.of(new RouteDetailsDTO(07.2, 300.3, null)),
+                    Arguments.of((RouteDetailsDTO) null)
+            );
+        }
+
+
+    }
+
 }
