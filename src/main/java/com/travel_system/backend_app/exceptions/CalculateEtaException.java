9d116e431cf04442817457ac167c195c51dec579
Iago-on-github <Iago737@outlook.com>
2026-05-11 08:38:17 -0300

    remove: - remoção de exception duplicada/desnecessária

--- src/main/java/com/travel_system/backend_app/exceptions/CalculateEtaException.java
+++
@@ -1,7 +0,0 @@
-package com.travel_system.backend_app.exceptions;
-
-public class CalculateEtaException extends RuntimeException {
-    public CalculateEtaException(String message) {
-        super(message);
-    }
-}
