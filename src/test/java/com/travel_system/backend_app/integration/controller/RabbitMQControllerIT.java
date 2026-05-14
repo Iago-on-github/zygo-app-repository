de9861d81ceec422905619d11bfd4b1285fb71e2
Iago-on-github <Iago737@outlook.com>
2026-05-14 11:55:29 -0300

    IT tests: - implementado os testes para os cenários da classe authenticateTopic

--- src/test/java/com/travel_system/backend_app/integration/controller/RabbitMQControllerIT.java
+++ src/test/java/com/travel_system/backend_app/integration/controller/RabbitMQControllerIT.java
@@ -4,8 +4,11 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.travel_system.backend_app.config.TokenConfig;
 import com.travel_system.backend_app.integration.IntegrationTestBase;
+import com.travel_system.backend_app.model.*;
 import com.travel_system.backend_app.model.enums.GeneralStatus;
-import com.travel_system.backend_app.repository.UserRepository;
+import com.travel_system.backend_app.model.enums.InstitutionType;
+import com.travel_system.backend_app.model.enums.TravelStatus;
+import com.travel_system.backend_app.repository.*;
 import com.travel_system.backend_app.service.TravelService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,14 +21,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.testcontainers.shaded.org.checkerframework.checker.guieffect.qual.UI;
 
 import java.time.Instant;
-import java.util.Date;
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.util.*;
 import java.util.stream.Stream;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -39,8 +42,17 @@ public class RabbitMQControllerIT extends IntegrationTestBase {
     @MockitoBean
     private UserRepository userRepository;
 
-    @Autowired
+    @MockitoBean
     private TravelService travelService;
+
+    @Autowired
+    private TravelRepository travelRepository;
+    @Autowired
+    private DriverRepository driverRepository;
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private StudentTravelRepository studentTravelRepository;
 
     @Value("${rabbitmq_user}")
     private String rabbitmq_user;
@@ -217,6 +229,123 @@ public class RabbitMQControllerIT extends IntegrationTestBase {
                             .param("vhost", "/")
                             .param("resource", "topic")
                             .param("name", "mocked_name")
+                            .param("permission", permission))
+                    .andExpect(content().string("deny"))
+                    .andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    class authenticateTopic {
+        Travel travel;
+        Driver driver;
+        StudentTravel studentTravel;
+        Student student;
+
+        @BeforeEach
+        void setUp() {
+            driver = new Driver(
+                    null, "driver@test.com", "encoded_pass",
+                    "João", "Silva", "71999999999",
+                    null, GeneralStatus.ACTIVE,
+                    LocalDateTime.now(), LocalDateTime.now(),
+                    "Salvador", 0, new ArrayList<>());
+            driverRepository.save(driver);
+
+            travel = new Travel(
+                    null, null, TravelStatus.TRAVELLING, driver,
+                    Instant.now(), null, "~shnC~_rcL_@v@m@p@y@r@",
+                    3600.0, 15000.0,
+                    -12.9714, -38.5016,
+                    -12.8000, -38.4000
+            );
+            travelRepository.save(travel);
+
+            student = new Student(
+                    null,
+                    "student@gmail.com",
+                    "senhaSegura123",
+                    "Student",
+                    "Teste",
+                    "75999999999",
+                    "teste_img",
+                    GeneralStatus.ACTIVE,
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    InstitutionType.UNIVERSITY,
+                    "Ciência da Computação"
+            );
+            studentRepository.save(student);
+
+            studentTravel = new StudentTravel(null, travel, student, true, Instant.now().minusSeconds(20), null, null);
+            studentTravelRepository.save(studentTravel);
+
+            travel.setStudentTravels(Set.of(studentTravel));
+        }
+
+        @Test
+        void shouldAllowDriverAuthenticateWithSuccess() throws Exception {
+            String permission = "publish";
+
+            String routingKey = "travel/" + travel.getId();
+
+            when(travelService.isDriverLogged(String.valueOf(driver.getId()), travel.getId())).thenReturn(true);
+
+            mockMvc.perform(post("/api/messaging/auth/topic")
+                            .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                            .param("user", String.valueOf(driver.getId()))
+                            .param("routing_key", routingKey)
+                            .param("permission", permission))
+                    .andExpect(content().string("allow"))
+                    .andExpect(status().isOk());
+        }
+
+        @Test
+        void shouldAllowStudentAuthenticateWithSuccess() throws Exception {
+            String permission = "subscribe";
+
+            String routingKey = "travel/" + travel.getId();
+
+            when(travelService.isStudentLogged(student.getId(), travel.getId())).thenReturn(true);
+
+            mockMvc.perform(post("/api/messaging/auth/topic")
+                            .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                            .param("user", String.valueOf(student.getId()))
+                            .param("routing_key", routingKey)
+                            .param("permission", permission))
+                    .andExpect(content().string("allow"))
+                    .andExpect(status().isOk());
+
+        }
+
+        @Test
+        @DisplayName("should deny and log error when exception occurs during topic auth")
+        void shouldNeverAllowWhenErrorOccursDuringAuthProcess() throws Exception {
+            // quando algum erro ocorrer durante o processo de verificação, deve cair no cair, não subir erro e retornar apenas deny
+            String permission = "subscribe";
+
+            String routingKey = "travel/" + travel.getId();
+
+            when(travelService.isStudentLogged(student.getId(), travel.getId())).thenThrow(new RuntimeException());
+
+            mockMvc.perform(post("/api/messaging/auth/topic")
+                            .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                            .param("user", String.valueOf(student.getId()))
+                            .param("routing_key", routingKey)
+                            .param("permission", permission))
+                    .andExpect(content().string("deny"))
+                    .andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("should deny when permission is neither publish or subscribe")
+        void shouldDenyUnknownPermission() throws Exception {
+            String permission = "configure"; // exemplo de permission inválida aqui
+
+            mockMvc.perform(post("/api/messaging/auth/topic")
+                            .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                            .param("user", UUID.randomUUID().toString())
+                            .param("routing_key", UUID.randomUUID().toString())
                             .param("permission", permission))
                     .andExpect(content().string("deny"))
                     .andExpect(status().isOk());
