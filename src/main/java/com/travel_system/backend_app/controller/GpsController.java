3bd48675d5663d4c0120650500e326073d2ce69a
Iago-on-github <Iago737@outlook.com>
2026-05-15 22:41:23 -0300

    fix: - conversão de String para UUID - chamada de repo com parâmetro convertido

--- src/main/java/com/travel_system/backend_app/controller/GpsController.java
+++ src/main/java/com/travel_system/backend_app/controller/GpsController.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/v1/gps")
 public class GpsController {
@@ -26,7 +28,9 @@ public class GpsController {
     @PostMapping("/updateGpsData")
     @PreAuthorize("hasRole('DRIVER')")
     public ResponseEntity<Void> vehicleGps(@RequestParam("city") String city, @RequestParam("travelId") String travelId, @RequestBody VehicleLocationRequestDTO vehicleLocation) {
-        boolean existsTravel = travelRepository.existsByIdAndTravelStatus(travelId, TravelStatus.TRAVELLING);
+        UUID travelConvertedId = UUID.fromString(travelId);
+
+        boolean existsTravel = travelRepository.existsByIdAndTravelStatus(travelConvertedId, TravelStatus.TRAVELLING);
 
         if (!existsTravel) {
             log.warn("Viagem não encontrada ou não está em andamento. Não envia nada ao rabbitmq: {} ", travelId);
