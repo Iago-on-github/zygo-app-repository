86fd66a03e6956a632184b46ac222c2da3b2fb61
Iago-on-github <Iago737@outlook.com>
2026-05-19 14:12:22 -0300

    remove: - remoção de exception não utilizada

--- src/main/java/com/travel_system/backend_app/exceptions/EmailNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.travel_system.backend_app.exceptions;
-
-public class EmailNotFoundException extends RuntimeException {
-    public EmailNotFoundException(String message) {
-        super(message);
-    }
-}
