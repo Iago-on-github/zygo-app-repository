eba85c062452a0a373aff5a3fab6dc620dc90544
Iago-on-github <Iago737@outlook.com>
2026-05-19 14:15:55 -0300

    fix: - remoção de métodos auxiliares para validar campos null - remoção de retorno 403 indevido - adição de class tokenService externa - métodos agr usam seus próprios responsesDTO

--- src/main/java/com/travel_system/backend_app/controller/AuthController.java
+++ src/main/java/com/travel_system/backend_app/controller/AuthController.java
@@ -1,54 +1,42 @@
 package com.travel_system.backend_app.controller;
 
+import com.travel_system.backend_app.config.TokenConfig;
 import com.travel_system.backend_app.model.dtos.request.LoginRequestDTO;
+import com.travel_system.backend_app.model.dtos.response.LoginResponseDTO;
+import com.travel_system.backend_app.model.dtos.response.RefreshTokenResponseDTO;
 import com.travel_system.backend_app.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/v1/auth")
 public class AuthController {
 
     private final AuthService authService;
+    private final TokenConfig tokenConfig;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, TokenConfig tokenConfig) {
         this.authService = authService;
+        this.tokenConfig = tokenConfig;
     }
 
     @PostMapping("/signing")
-    public ResponseEntity signing(@RequestBody LoginRequestDTO data) {
-        if (checkParamsIsNotNull(data)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Falta email ou senha.");
-
+    public ResponseEntity<LoginResponseDTO> signing(@Valid @RequestBody LoginRequestDTO data) {
         var token = authService.signing(data);
-        if (token == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Falha na requisição do cliente.");
 
         return ResponseEntity.ok().body(token);
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity refreshToken(Authentication auth, @RequestHeader("Authorization") String refreshToken) {
-        String email = auth.getName(); // email do user JÁ logado
-
-        if (checkParamsIsNotNull(email, refreshToken)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Falta email ou senha.");
+    public ResponseEntity<RefreshTokenResponseDTO> refreshToken(@RequestHeader("X-Refresh-Token") String refreshToken) {
+        String email = tokenConfig.getSubjectFromToken(refreshToken);
 
         var token = authService.refreshToken(email, refreshToken);
-        if (token == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Falha na requisição do cliente.");
 
         return ResponseEntity.ok().body(token);
     }
-
-//    MÉTODOS AUXILIARES
-//    MÉTODOS AUXILIARES
-//    MÉTODOS AUXILIARES
-
-    private boolean checkParamsIsNotNull(LoginRequestDTO data) {
-        return data.email() == null || data.password() == null || data.email().isBlank() || data.password().isBlank();
-    }
-
-    private boolean checkParamsIsNotNull(String email, String refreshToken) {
-        return email == null || refreshToken == null || email.isBlank() || refreshToken.isBlank();
-    }
-
 }
