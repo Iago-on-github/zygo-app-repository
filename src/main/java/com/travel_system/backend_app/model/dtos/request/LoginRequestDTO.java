359710a2b02ec2ed9f53c3c5844809f526953dee
Iago-on-github <Iago737@outlook.com>
2026-05-19 14:12:05 -0300

    update: - anotações do beanValidation inseridas no DTO de response do login

--- src/main/java/com/travel_system/backend_app/model/dtos/request/LoginRequestDTO.java
+++ src/main/java/com/travel_system/backend_app/model/dtos/request/LoginRequestDTO.java
@@ -1,4 +1,12 @@
 package com.travel_system.backend_app.model.dtos.request;
 
-public record LoginRequestDTO(String email, String password) {
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public record LoginRequestDTO(
+        @Email(message = "insira um email válido")
+        @NotBlank(message = "email obrigatório")
+        String email,
+        @NotBlank(message = "senha obrigatória")
+        String password) {
 }
