bdc2ee8d82793ab3cc4b0ba3b380ed029618a7d1
Iago-on-github <Iago737@outlook.com>
2026-05-19 17:28:33 -0300

    feat: - adição de DTO de update único para atualização da entidade Driver

---
+++ src/main/java/com/travel_system/backend_app/model/dtos/request/DriverUpdateDTO.java
@@ -0,0 +1,18 @@
+package com.travel_system.backend_app.model.dtos.request;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+public record DriverUpdateDTO(
+        @Email
+        String email,
+        @Size(min = 7)
+        String password,
+        String name,
+        String lastName,
+        String telephone,
+        String profilePicture,
+        String areaOfActivity
+) {
+}
