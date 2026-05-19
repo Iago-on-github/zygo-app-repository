4d69c29c40a70adb878e3569c9cc61e5c1c685a8
Iago-on-github <Iago737@outlook.com>
2026-05-18 22:19:42 -0300

    feat: - criação de customização para a documentação do Swagger

---
+++ src/main/java/com/travel_system/backend_app/config/OpenAPIConfig.java
@@ -0,0 +1,22 @@
+package com.travel_system.backend_app.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Zygo API - Travel-System")
+                        .version("v1")
+                        .description("Zygo's API description")
+                        .license(new License()
+                                .name("Proprietary")));
+    }
+}
