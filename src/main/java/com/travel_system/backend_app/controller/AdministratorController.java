9941543e0f50341dea5e5ee1b863a7c5651dae21
Iago-on-github <Iago737@outlook.com>
2026-05-18 22:21:51 -0300

    feat/fix: - remoção de métodos depreciados no controller - mapeamento dos novos métodos

--- src/main/java/com/travel_system/backend_app/controller/AdministratorController.java
+++ src/main/java/com/travel_system/backend_app/controller/AdministratorController.java
@@ -2,7 +2,9 @@ package com.travel_system.backend_app.controller;
 
 import com.travel_system.backend_app.model.dtos.request.AdministratorRequestDTO;
 import com.travel_system.backend_app.model.dtos.request.AdministratorUpdateDTO;
+import com.travel_system.backend_app.model.dtos.request.UpdateAdministratorStatusDTO;
 import com.travel_system.backend_app.model.dtos.response.AdministratorResponseDTO;
+import com.travel_system.backend_app.model.enums.GeneralStatus;
 import com.travel_system.backend_app.service.AdministratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +17,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/admins")
+@RequestMapping("/v1/admins")
 public class AdministratorController {
 
     private final AdministratorService administratorService;
@@ -29,23 +31,18 @@ public class AdministratorController {
         return ResponseEntity.ok().body(administratorService.getAllAdministrators());
     }
 
-    @GetMapping("/active")
-    public ResponseEntity<List<AdministratorResponseDTO>> getAllActiveAdmins() {
-        return ResponseEntity.ok().body(administratorService.getAllActiveAdministrators());
+    @GetMapping
+    public ResponseEntity<List<AdministratorResponseDTO>> getAdminsByStatus(@RequestParam(required = false) GeneralStatus status) {
+        return ResponseEntity.ok().body(administratorService.getAllAdministratorsByStatus(status));
     }
 
-    @GetMapping("/inactive")
-    public ResponseEntity<List<AdministratorResponseDTO>> getAllInactiveAdmins() {
-        return ResponseEntity.ok().body(administratorService.getAllInactiveAdministrators());
-    }
-
-    @GetMapping("/logged")
-    public ResponseEntity<AdministratorResponseDTO> getLoggedAdministratorInProfile(Authentication auth) {
+    @GetMapping("/me")
+    public ResponseEntity<AdministratorResponseDTO> getCurrentAdministrator(Authentication auth) {
         String authEmail = auth.getName();
-        return ResponseEntity.ok().body(administratorService.getLoggedAdministratorInProfile(authEmail));
+        return ResponseEntity.ok().body(administratorService.getCurrentAdministrator(authEmail));
     }
 
-    @PostMapping("/new")
+    @PostMapping
     public ResponseEntity<AdministratorResponseDTO> createAdministrator(@RequestBody AdministratorRequestDTO admRequestDTO, UriComponentsBuilder componentsBuilder) {
         AdministratorResponseDTO newAdm = administratorService.createAdministrator(admRequestDTO);
 
@@ -54,21 +51,15 @@ public class AdministratorController {
         return ResponseEntity.created(uri).body(newAdm);
     }
 
-    @PatchMapping("/update")
-    public ResponseEntity<AdministratorResponseDTO> updateLoggedAdministrator(@RequestBody AdministratorUpdateDTO administratorUpdateDto, Authentication auth) {
+    @PatchMapping("/me")
+    public ResponseEntity<AdministratorResponseDTO> updateCurrentAdministrator(@RequestBody AdministratorUpdateDTO administratorUpdateDto, Authentication auth) {
         String authEmail = auth.getName();
-        return ResponseEntity.ok().body(administratorService.updateLoggedAdministrator(authEmail, administratorUpdateDto));
+        return ResponseEntity.ok().body(administratorService.updateCurrentAdministrator(authEmail, administratorUpdateDto));
     }
 
-    @PutMapping("/disable/{id}")
-    public ResponseEntity<Void> disableAdministrator(@PathVariable UUID id) {
-        administratorService.disableAdministrator(id);
-        return ResponseEntity.noContent().build();
-    }
-
-    @PutMapping("/enable/{id}")
-    public ResponseEntity<Void> enableAdministrator(@PathVariable UUID id) {
-        administratorService.enableAdministrator(id);
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> updateAdministrator(@PathVariable UUID id, @RequestBody UpdateAdministratorStatusDTO administratorStatusDTO) {
+        administratorService.updateAdministrator(id, administratorStatusDTO.status());
         return ResponseEntity.noContent().build();
     }
 
