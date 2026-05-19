9ac57edbf0996c243090f9e8a57ac8dbab8b0d70
Iago-on-github <Iago737@outlook.com>
2026-05-19 17:25:58 -0300

    update: - adição de campos de validação nas props de email e password

--- src/main/java/com/travel_system/backend_app/model/dtos/request/DriverRequestDTO.java
+++ src/main/java/com/travel_system/backend_app/model/dtos/request/DriverRequestDTO.java
@@ -2,14 +2,21 @@ package com.travel_system.backend_app.model.dtos.request;
 
 import com.travel_system.backend_app.model.enums.GeneralStatus;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public record DriverRequestDTO(
+        @Email
+        @NotBlank
         String email,
+        @Size(min = 7)
         String password,
         String name,
         String lastName,
         String telephone,
         String profilePicture,
-        GeneralStatus status,
         String areaOfActivity
 ) {
 }
