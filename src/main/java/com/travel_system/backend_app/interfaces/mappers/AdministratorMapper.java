dd252264c581e35b1ad04a0eed524c88fd1f6542
Iago-on-github <Iago737@outlook.com>
2026-05-20 17:49:35 -0300

    feat: - adicionado anotação para ignorar a senha

--- src/main/java/com/travel_system/backend_app/interfaces/mappers/AdministratorMapper.java
+++ src/main/java/com/travel_system/backend_app/interfaces/mappers/AdministratorMapper.java
@@ -2,14 +2,12 @@ package com.travel_system.backend_app.interfaces.mappers;
 
 import com.travel_system.backend_app.model.Administrator;
 import com.travel_system.backend_app.model.dtos.request.AdministratorUpdateDTO;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface AdministratorMapper {
 
+    @Mapping(target = "password", ignore = true)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void administratorUpdateFromDTO(AdministratorUpdateDTO admUpdateDTO, @MappingTarget Administrator admEntity);
 }
