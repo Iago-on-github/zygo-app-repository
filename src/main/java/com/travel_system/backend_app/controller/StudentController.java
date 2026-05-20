d2a18e51b8ca836181beee099db98f5ed4fff8f0
Iago-on-github <Iago737@outlook.com>
2026-05-20 17:42:26 -0300

    fix/feat: - removidos alguns endpoints depreciados - endpoints individuais que faziam busca foram refatorados por endpoint único que faz a ação baseada no parâmetro - adicionado, nos endpoints de escrita, a anotação valid, do beanValidation  - adicionado novo DTO de update, no método de update, no lugar do request padrão

--- src/main/java/com/travel_system/backend_app/controller/StudentController.java
+++ src/main/java/com/travel_system/backend_app/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.travel_system.backend_app.controller;
 
 import com.travel_system.backend_app.model.dtos.request.StudentRequestDTO;
+import com.travel_system.backend_app.model.dtos.request.StudentUpdateDTO;
+import com.travel_system.backend_app.model.dtos.request.UpdateEntityStatusDTO;
 import com.travel_system.backend_app.model.dtos.response.StudentResponseDTO;
+import com.travel_system.backend_app.model.enums.GeneralStatus;
 import com.travel_system.backend_app.service.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -14,53 +17,51 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/students")
+@RequestMapping("/v1/students")
 public class StudentController {
     private final StudentService studentService;
 
-    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<List<StudentResponseDTO>> getAllStudents() {
         return ResponseEntity.ok().body(studentService.getAllStudents());
     }
 
-    @GetMapping("/active")
-    public ResponseEntity<List<StudentResponseDTO>> getAllActiveStudents() {
-        return ResponseEntity.ok().body(studentService.getAllActiveStudents());
+    @GetMapping
+    public ResponseEntity<List<StudentResponseDTO>> getStudentsByStatus(@RequestParam(required = false) GeneralStatus status) {
+        return ResponseEntity.ok().body(studentService.getStudentsByStatus(status));
     }
 
-    @GetMapping("/inactive")
-    public ResponseEntity<List<StudentResponseDTO>> getAllInactiveStudents() {
-        return ResponseEntity.ok().body(studentService.getAllInactiveStudents());
-    }
-
-    @GetMapping("/logged")
-    public ResponseEntity<StudentResponseDTO> getLoggedInStudentProfile(Authentication auth) {
+    @GetMapping("/me")
+    public ResponseEntity<StudentResponseDTO> getCurrentStudent(Authentication auth) {
         String email = auth.getName();
-        return ResponseEntity.ok().body(studentService.getLoggedInStudentProfile(email));
+
+        return ResponseEntity.ok().body(studentService.getCurrentStudent(email));
     }
 
-    @PostMapping("/new")
-    public ResponseEntity<StudentResponseDTO> createStudent(@RequestBody StudentRequestDTO studentRequestDTO, UriComponentsBuilder componentsBuilder) {
+    @PostMapping
+    public ResponseEntity<StudentResponseDTO> createStudent(@Valid @RequestBody StudentRequestDTO studentRequestDTO, UriComponentsBuilder componentsBuilder) {
         StudentResponseDTO student = studentService.createStudent(studentRequestDTO);
+
         URI uri = componentsBuilder.path("{/id}").buildAndExpand(student.id()).toUri();
+
         return ResponseEntity.created(uri).body(student);
     }
 
-    @PutMapping("/update")
-    public ResponseEntity<StudentResponseDTO> updateLoggedStudent(Authentication auth, @RequestBody StudentRequestDTO studentRequestDTO) {
+    @PatchMapping("/me")
+    public ResponseEntity<StudentResponseDTO> updateCurrentStudent(Authentication auth, @Valid @RequestBody StudentUpdateDTO studentUpdateDTO) {
         String email = auth.getName();
-        return ResponseEntity.ok().body(studentService.updateLoggedStudent(email, studentRequestDTO));
+
+        return ResponseEntity.ok().body(studentService.updateCurrentStudent(email, studentUpdateDTO));
     }
 
-    @PutMapping("/disable/{id}")
-    public ResponseEntity<Void> disableStudent(@PathVariable UUID id) {
-        studentService.disableStudent(id);
-        return ResponseEntity.ok().build();
+    @PatchMapping("/{studentId}")
+    public ResponseEntity<Void> updateStudentStatus(@PathVariable UUID studentId, @Valid @RequestBody UpdateEntityStatusDTO newStudentStatus) {
+        studentService.updateStudentStatus(studentId, newStudentStatus.status());
+        return ResponseEntity.noContent().build();
     }
 
 }
