c21cecfcf29b0586f675c8e2c96f9490d3152aa0
Iago-on-github <Iago737@outlook.com>
2026-05-20 17:40:15 -0300

    feat: - adicionado validações com BeanValidation para alguns campos do dto

--- src/main/java/com/travel_system/backend_app/model/dtos/request/StudentRequestDTO.java
+++ src/main/java/com/travel_system/backend_app/model/dtos/request/StudentRequestDTO.java
@@ -2,11 +2,20 @@ package com.travel_system.backend_app.model.dtos.request;
 
 import com.travel_system.backend_app.model.enums.InstitutionType;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public record StudentRequestDTO(
+        @Email
+        @NotBlank
         String email,
+        @Size(min = 7)
         String password,
+        @Size(min = 4)
         String name,
         String lastName,
+        @Size(min = 9)
         String telephone,
         String profilePicture,
         InstitutionType institutionType,
