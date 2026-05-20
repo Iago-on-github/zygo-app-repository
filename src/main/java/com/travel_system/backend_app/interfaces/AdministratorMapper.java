aa8d6d8c12fc56128c3b83e95e37652348ac49b7
Iago-on-github <Iago737@outlook.com>
2026-05-20 09:22:57 -0300

    Revert "feat:"
    This reverts commit a6d78aac5498d53b108762ad448d72d2abb7b04b.

--- src/main/java/com/travel_system/backend_app/interfaces/AdministratorMapper.java
+++ src/main/java/com/travel_system/backend_app/interfaces/AdministratorMapper.java
@@ -1,13 +1,15 @@
-package com.travel_system.backend_app.interfaces.mappers;
+package com.travel_system.backend_app.interfaces;
 
 import com.travel_system.backend_app.model.Administrator;
 import com.travel_system.backend_app.model.dtos.request.AdministratorUpdateDTO;
-import org.mapstruct.*;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface AdministratorMapper {
 
-    @Mapping(target = "password", ignore = true)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void administratorUpdateFromDTO(AdministratorUpdateDTO admUpdateDTO, @MappingTarget Administrator admEntity);
 }
