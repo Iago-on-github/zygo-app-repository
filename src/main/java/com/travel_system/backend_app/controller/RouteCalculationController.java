1df73fefb1d7e425b458a62823c6539d63f4b8eb
Iago-on-github <Iago737@outlook.com>
2026-05-20 17:45:18 -0300

    feat/fix - pequenas refatorações no mapping do endpoint - troca de propriedade soltas para DTO fixo

--- src/main/java/com/travel_system/backend_app/controller/RouteCalculationController.java
+++ src/main/java/com/travel_system/backend_app/controller/RouteCalculationController.java
@@ -1,17 +1,16 @@
 package com.travel_system.backend_app.controller;
 
 import com.travel_system.backend_app.model.dtos.mapboxApi.RouteDeviationDTO;
+import com.travel_system.backend_app.model.dtos.request.RouteDeviationRequestDTO;
 import com.travel_system.backend_app.service.RouteCalculationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/routeCalculation")
+@RequestMapping("/v1/route")
 public class RouteCalculationController {
     private final RouteCalculationService routeCalculationService;
 
@@ -19,9 +18,9 @@ public class RouteCalculationController {
         this.routeCalculationService = routeCalculationService;
     }
 
-    @GetMapping("/deviation")
-    public ResponseEntity<RouteDeviationDTO> isRouteDeviation(@RequestParam Double currentLat, @RequestParam Double currentLong, @RequestParam String polylineRoute) {
-        RouteDeviationDTO deviationDTO = routeCalculationService.isRouteDeviation(currentLat, currentLong, polylineRoute);
+    @PostMapping("/deviation")
+    public ResponseEntity<RouteDeviationDTO> isRouteDeviation(@RequestBody RouteDeviationRequestDTO routeDeviationRequestDTO) {
+        RouteDeviationDTO deviationDTO = routeCalculationService.isRouteDeviation(routeDeviationRequestDTO);
         return ResponseEntity.ok().body(deviationDTO);
     }
 }
