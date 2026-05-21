fedfb1726e62090a7d621464ca829e79f1d8c71b
Iago-on-github <Iago737@outlook.com>
2026-05-21 09:13:21 -0300

    fix: - pequena refatoração nos mappings dos endpoints - agora os endpoints de join e end travel levarão como base o student logado para as suas ações

--- src/main/java/com/travel_system/backend_app/controller/TravelController.java
+++ src/main/java/com/travel_system/backend_app/controller/TravelController.java
@@ -3,8 +3,8 @@ package com.travel_system.backend_app.controller;
 import com.travel_system.backend_app.model.dtos.request.TravelRequestDTO;
 import com.travel_system.backend_app.model.dtos.response.TravelResponseDTO;
 import com.travel_system.backend_app.service.TravelService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -12,7 +12,7 @@ import java.net.URI;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/travel")
+@RequestMapping("/v1/travel")
 public class TravelController {
 
     private final TravelService travelService;
@@ -24,32 +24,37 @@ public class TravelController {
     @PostMapping("/create")
     public ResponseEntity<TravelResponseDTO> createTravel(@RequestBody TravelRequestDTO travelRequestDTO, UriComponentsBuilder componentsBuilder) {
         TravelResponseDTO responseDTO = travelService.createTravel(travelRequestDTO);
+
         URI uri = componentsBuilder.path("/{id}").buildAndExpand(responseDTO.id()).toUri();
 
         return ResponseEntity.created(uri).body(responseDTO);
     }
 
-    @PostMapping("/start/{travelId}")
+    @PostMapping("/{travelId}/start")
     public ResponseEntity<Void> startTravel(@PathVariable UUID travelId) {
         travelService.startTravel(travelId);
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/end/{travelId}")
+    @PostMapping("/{travelId}/end")
     public ResponseEntity<Void> endTravel(@PathVariable UUID travelId) {
         travelService.endTravel(travelId);
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/join/{travelId}/{studentId}")
-    public ResponseEntity<Void> joinTravel(@PathVariable UUID travelId, @PathVariable UUID studentId) {
-        travelService.joinTravel(travelId, studentId);
+    @PostMapping("/{travelId}/join")
+    public ResponseEntity<Void> joinTravel(@PathVariable UUID travelId, Authentication authentication) {
+        String studentEmail = authentication.getName(); // email do student logado
+
+        travelService.joinTravel(travelId, studentEmail);
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/leave/{travelId}/{studentId}")
-    public ResponseEntity<Void> leaveTravel (@PathVariable UUID travelId, @PathVariable UUID studentId) {
-        travelService.leaveTravel(travelId, studentId);
+    @PostMapping("/{travelId}/leave")
+    public ResponseEntity<Void> leaveTravel (@PathVariable UUID travelId, Authentication authentication) {
+        String studentEmail = authentication.getName(); // email do student logado
+
+        travelService.leaveTravel(travelId, studentEmail);
         return ResponseEntity.noContent().build();
     }
 }
