1f6fc64112e4591d7aadfee0fde19fc4532d7470
Iago-on-github <Iago737@outlook.com>
2026-05-28 14:26:07 -0300

    feat: - novo enum de status para a entidade studentTravel adicionado

---
+++ src/main/java/com/travel_system/backend_app/model/enums/StudentTravelStatus.java
@@ -0,0 +1,9 @@
+package com.travel_system.backend_app.model.enums;
+
+// status que o sistema usa para validar se ainda está na viagem
+public enum StudentTravelStatus {
+    ACTIVE,
+    AWAY_FROM_BUS,
+    AUTO_DISCONNECTED,
+    LEFT
+}
