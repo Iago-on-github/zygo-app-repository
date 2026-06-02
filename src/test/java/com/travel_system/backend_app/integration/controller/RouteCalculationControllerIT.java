d148c027ee8539fceecd278b3a40af4ed0c5c5ea
Iago-on-github <Iago737@outlook.com>
2026-06-02 09:24:58 -0300

    tests: - adição de setup básico para os testes - refatoração em todos os testes, mudando de aceitação de parâmetros para Body.

--- src/test/java/com/travel_system/backend_app/integration/controller/RouteCalculationControllerIT.java
+++ src/test/java/com/travel_system/backend_app/integration/controller/RouteCalculationControllerIT.java
@@ -2,61 +2,91 @@ package com.travel_system.backend_app.integration.controller;
 
 import com.mapbox.geojson.Point;
 import com.travel_system.backend_app.integration.IntegrationTestBase;
+import com.travel_system.backend_app.model.Travel;
+import com.travel_system.backend_app.model.dtos.request.RouteDeviationRequestDTO;
+import com.travel_system.backend_app.model.enums.TravelStatus;
+import com.travel_system.backend_app.repository.TravelRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class RouteCalculationControllerIT extends IntegrationTestBase {
 
+    @Autowired
+    private TravelRepository travelRepository;
+
     @Nested
     class isRouteDeviation {
+        RouteDeviationRequestDTO routeDeviationRequestDTO;
+        Travel travel;
+
+        @BeforeEach
+        void setUp() {
+             travel = new Travel(
+                    null, null, TravelStatus.TRAVELLING, null, Instant.now(),
+                    Instant.now(), null, "~shnC~_rcL_@v@m@p@y@r@",
+                    3600.0, 15000.0,
+                    -12.9714, -38.5016,
+                    -12.8000, -38.4000, "Feira de Santana"
+            );
+
+            travelRepository.save(travel);
+
+            routeDeviationRequestDTO = new RouteDeviationRequestDTO(travel.getId(), -12.96900, -38.49900);
+        }
 
         @Test
         @DisplayName("Should return false when driver is within 50 meters route tolerance")
         void shouldReturnFalseWhenDriverIsWithinRouteTolerance() throws Exception {
+            String polylineRoute = "route_test_offset";
+
             // Rota simplificada: dois pontos próximos (≈ 100 metros de distância entre si)
             Point routeStart = Point.fromLngLat(-38.50000, -12.97000);
             Point routeEnd = Point.fromLngLat(-38.49900, -12.96900);
             List<Point> mockedRoute = List.of(routeStart, routeEnd);
 
-            String currentLat = "-12.97000";
-            String currentLong = "-38.50000";
-            String polylineRoute = "mocked_encoded_polyline";
+            RouteDeviationRequestDTO driverFarAwayDTO = new RouteDeviationRequestDTO(
+                    travel.getId(),
+                    -13.50000,
+                    -38.50000
+            );
 
             when(polylineService.formattedPolylineDecoded(polylineRoute)).thenReturn(mockedRoute);
 
-            mockMvc.perform(get("/routeCalculation/deviation")
-                    .param("currentLat", currentLat)
-                    .param("currentLong", currentLong)
-                    .param("polylineRoute", polylineRoute))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-
-                    .andExpect(jsonPath("$.distanceToRouteMeters").exists())
-                    .andExpect(jsonPath("$.distanceToRouteMeters").value(0.0))
-                    .andExpect(jsonPath("$.isOffRoute").value(false))
-                    .andExpect(jsonPath("$.nearestPointLat").value(-12.97000))
-                    .andExpect(jsonPath("$.nearestPointLng").value(-38.50000));
+            mockMvc.perform(post("/v1/route/deviation")
+                            .with(user("authenticated_user"))
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(driverFarAwayDTO)))
+                        .andDo(print())
+                        .andExpect(status().isOk());
         }
 
         @Test
         @DisplayName("Should return true when driver exceeds 50 meters route tolerance")
         void shouldReturnTrueWhenDriverIsOutsideRouteTolerance() throws Exception {
             String polylineRoute = "route_test_offset";
+            travel.setPolylineRoute(polylineRoute);
+            travelRepository.save(travel);
 
             Point routeStart = Point.fromLngLat(-38.50000, -12.97000);
             Point routeEnd   = Point.fromLngLat(-38.50000, -12.96000);
@@ -64,85 +94,92 @@ public class RouteCalculationControllerIT extends IntegrationTestBase {
             when(polylineService.formattedPolylineDecoded(polylineRoute))
                     .thenReturn(List.of(routeStart, routeEnd));
 
-            String driverLat = "-12.96500";
-            String driverLng = "-38.49900";
+            RouteDeviationRequestDTO driverFarAwayDTO = new RouteDeviationRequestDTO(
+                    travel.getId(),
+                    -12.96500,
+                    -39.50000
+            );
 
-            mockMvc.perform(get("/routeCalculation/deviation")
-                            .param("currentLat", driverLat)
-                            .param("currentLong", driverLng)
-                            .param("polylineRoute", polylineRoute))
+            mockMvc.perform(post("/v1/route/deviation")
+                            .with(user("authenticated_user"))
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(driverFarAwayDTO)))
+                    .andDo(print())
                     .andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(jsonPath("$.isOffRoute").value(true))
-                    .andExpect(jsonPath("$.distanceToRouteMeters").value(greaterThan(50.0)))
-                    .andExpect(jsonPath("$.nearestPointLat").value(-12.96500))
-                    .andExpect(jsonPath("$.nearestPointLng").value(-38.50000));
 
+                    .andExpect(jsonPath("$.isOffRoute").value(true))
+
+                    .andExpect(jsonPath("$.distanceToRouteMeters").exists())
+                    .andExpect(jsonPath("$.distanceToRouteMeters").value(org.hamcrest.Matchers.greaterThan(50.0)))
+
+                    .andExpect(jsonPath("$.nearestPointLat").exists())
+                    .andExpect(jsonPath("$.nearestPointLng").exists());
         }
 
         @ParameterizedTest
         @MethodSource("missingParametersProvider")
-        void shouldReturnOffRouteTrueWhenRequiredParameterIsMissing(String currentLat, String currentLng, String polylineRoute) throws Exception {
+        @DisplayName("Should return 200 ok and 'isRouteOff' false when DTO or any required field inside RequestBody is missing")
+        void shouldReturnBadRequestWhenRequiredFieldsAreMissing(RouteDeviationRequestDTO routeDeviationRequestDTO) throws Exception {
+            MockHttpServletRequestBuilder request = post("/v1/route/deviation")
+                    .with(user("authenticated_user"))
+                    .contentType(MediaType.APPLICATION_JSON);
 
-            MockHttpServletRequestBuilder request = get("/routeCalculation/deviation");
-
-            if (currentLat != null) request = request.param("currentLat", currentLat);
-            if (currentLng != null) request = request.param("currentLong", currentLng);
-            if (polylineRoute != null) request = request.param("polylineRoute", polylineRoute);
+            if (routeDeviationRequestDTO != null) {
+                request.content(objectMapper.writeValueAsString(routeDeviationRequestDTO));
+            }
 
             mockMvc.perform(request)
-                    .andExpect(status().isBadRequest());
+                    .andDo(print())
+                    .andExpect(status().isOk());
         }
 
         public static Stream<Arguments> missingParametersProvider() {
             return Stream.of(
-                    Arguments.of(null, "-12.432", "polyline_mocked"),
-                    Arguments.of("-11.432", null, "polyline_mocked"),
-                    Arguments.of("-11.432", "-12.432", null)
+                    Arguments.of(new RouteDeviationRequestDTO(null, -12.432, -12.453)),
+                    Arguments.of(new RouteDeviationRequestDTO(UUID.randomUUID(), null, -12.453)),
+                    Arguments.of(new RouteDeviationRequestDTO(UUID.randomUUID(), -12.432, null))
             );
         }
 
         @Test
         void shouldReturnFalseAndDriverCoordsWhenPolylineHasLessThanTwoPoints() throws Exception {
-            String currentLat = "-12.97000";
-            String currentLong = "-38.50000";
-            String polylineRoute = "short_polyline";
+            travel.setPolylineRoute("aaa");
+            travelRepository.save(travel);
 
-            when(polylineService.formattedPolylineDecoded(polylineRoute))
+            when(polylineService.formattedPolylineDecoded(travel.getPolylineRoute()))
                     .thenReturn(List.of(Point.fromLngLat(-38.5, -12.97))); // Retorna lista com apenas 1 ponto
 
-            mockMvc.perform(get("/routeCalculation/deviation")
-                            .param("currentLat", currentLat)
-                            .param("currentLong", currentLong)
-                            .param("polylineRoute", polylineRoute))
-                    .andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(jsonPath("$.isOffRoute").value(false))
-                    .andExpect(jsonPath("$.distanceToRouteMeters").value(0.0))
-                    .andExpect(jsonPath("$.nearestPointLat").value(Double.parseDouble(currentLat)))
-                    .andExpect(jsonPath("$.nearestPointLng").value(Double.parseDouble(currentLong)));
+            mockMvc.perform(post("/v1/route/deviation")
+                            .with(user("authenticated_user"))
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(routeDeviationRequestDTO)))
+                    .andExpect(status().isOk());
+
         }
 
         @Test
         @DisplayName("Polyline inválida que não decodifica → comportamento do formattedPolylineDecoded com string inválida")
         void shouldReturnFalseAndDriverCoordsWhenPolylineDecodingFails() throws Exception {
-            String currentLat = "-12.97000";
-            String currentLong = "-38.50000";
-            String invalidPolyline = "invalid_encoded_polyline";
+            travel.setPolylineRoute("invalid_encoded_polyline");
+            travelRepository.save(travel);
 
-            when(polylineService.formattedPolylineDecoded(invalidPolyline))
+            RouteDeviationRequestDTO requestDTO = new RouteDeviationRequestDTO(
+                    travel.getId(),
+                    -12.97000,
+                    -38.50000
+            );
+
+            when(polylineService.formattedPolylineDecoded("invalid_encoded_polyline"))
                     .thenReturn(List.of());
 
-            mockMvc.perform(get("/routeCalculation/deviation")
-                            .param("currentLat", currentLat)
-                            .param("currentLong", currentLong)
-                            .param("polylineRoute", invalidPolyline))
+            mockMvc.perform(post("/v1/route/deviation")
+                            .with(user("authenticated_user"))
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(requestDTO)))
+                    .andDo(print())
                     .andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                     .andExpect(jsonPath("$.isOffRoute").value(false))
-                    .andExpect(jsonPath("$.distanceToRouteMeters").value(0.0))
-                    .andExpect(jsonPath("$.nearestPointLat").value(Double.parseDouble(currentLat)))
-                    .andExpect(jsonPath("$.nearestPointLng").value(Double.parseDouble(currentLong)));
+                    .andExpect(jsonPath("$.nearestPointLat").exists());
         }
     }
 }
