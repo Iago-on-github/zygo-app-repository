55aee5f5f45e9cd8d4c03be3e0c93ec765dabd18
Iago-on-github <Iago737@outlook.com>
2026-06-03 10:29:01 -0300

    feat: - anotação do beanvalidation para que as anotações de validação prévia no dto sejam validas

--- src/main/java/com/travel_system/backend_app/controller/TravelTrackingController.java
+++ src/main/java/com/travel_system/backend_app/controller/TravelTrackingController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
@@ -24,7 +25,7 @@ public class TravelTrackingController {
     }
 
     @PostMapping("/travels/{travelId}/locations/{cityId}")
-    public ResponseEntity<Void> markDriverCheckpoint(@PathVariable UUID cityId, @PathVariable UUID travelId, @RequestBody VehicleLocationRequestDTO vehicleLocationRequest) {
+    public ResponseEntity<Void> markDriverCheckpoint(@PathVariable UUID cityId, @PathVariable UUID travelId, @Valid @RequestBody VehicleLocationRequestDTO vehicleLocationRequest) {
         travelTrackingService.markDriverCheckpoint(cityId, travelId, vehicleLocationRequest);
         return ResponseEntity.ok().build();
     }
