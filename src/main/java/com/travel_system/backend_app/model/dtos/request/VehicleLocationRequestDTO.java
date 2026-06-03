1aaf280ec5d113cd0b66188d3bea70ffc6d1d963
Iago-on-github <Iago737@outlook.com>
2026-06-03 10:27:46 -0300

    feat: - campos de latitude e longitude do dto são validados com BeanValidation

--- src/main/java/com/travel_system/backend_app/model/dtos/request/VehicleLocationRequestDTO.java
+++ src/main/java/com/travel_system/backend_app/model/dtos/request/VehicleLocationRequestDTO.java
@@ -1,10 +1,11 @@
 package com.travel_system.backend_app.model.dtos.request;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 public record VehicleLocationRequestDTO(UUID travelId,
-                                        Double latitude,
-                                        Double longitude,
+                                        @NotNull Double latitude,
+                                        @NotNull Double longitude,
                                         Double speed,
                                         Double heading) {
 }
