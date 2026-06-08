86f8c3b5bab557050e6d0cc3d446cd03fbf64306
Iago-on-github <Iago737@outlook.com>
2026-06-08 17:30:42 -0300

    feat: - adição de config para requisições com auth no Swagger

--- src/main/java/com/travel_system/backend_app/config/OpenAPIConfig.java
+++ src/main/java/com/travel_system/backend_app/config/OpenAPIConfig.java
@@ -1,8 +1,10 @@
 package com.travel_system.backend_app.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +19,13 @@ public class OpenAPIConfig {
                         .version("v1")
                         .description("Zygo's API description")
                         .license(new License()
-                                .name("Proprietary")));
+                                .name("Proprietary")))
+
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
