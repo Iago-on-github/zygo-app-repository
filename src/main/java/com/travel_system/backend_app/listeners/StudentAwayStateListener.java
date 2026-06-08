23df7f7935a0baad971fd567f41fda643904bc00
Iago-on-github <Iago737@outlook.com>
2026-06-08 16:31:53 -0300

    feat: - Criação de listener para lidar com o processamento async de eventos para o algoritmo de auto-desvínculo de estudantes. - adição de estratégia de Lock para evitar múltiplos processamentos de um mesmo dado por conta de falhas ou lentidões na execução do alg

---
+++ src/main/java/com/travel_system/backend_app/listeners/StudentAwayStateListener.java
@@ -0,0 +1,43 @@
+package com.travel_system.backend_app.listeners;
+
+import com.travel_system.backend_app.events.StudentAwayStateCheckEvent;
+import com.travel_system.backend_app.service.LocationService;
+import com.travel_system.backend_app.service.RedisTrackingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentAwayStateListener {
+
+    private final LocationService locationService;
+    private final RedisTrackingService redisTrackingService;
+
+    private final Logger logger = LoggerFactory.getLogger(StudentAwayStateListener.class);
+
+    public StudentAwayStateListener(LocationService locationService, RedisTrackingService redisTrackingService) {
+        this.locationService = locationService;
+        this.redisTrackingService = redisTrackingService;
+    }
+
+    @Async("studentAwayTaskExecutor")
+    @EventListener
+    public void handleStudentAwayState(StudentAwayStateCheckEvent event) {
+        // usa redis para garantir que a demora na execução do algoritmo não gere múltiplos processamentos, evitando duplicidade
+        boolean isLockAcquire = redisTrackingService.tryAcquireStudentAwayStateLock(event.travelId());
+
+        if (!isLockAcquire) {
+            logger.debug("[handleStudentAwayState] viagem {} já está sendo processada.", event.travelId());
+            return;
+        }
+        try {
+            locationService.processStudentAwayState(event);
+        } catch (Exception e) {
+            logger.error("[handleStudentAwayState] - não foi possível publicar o evento para a viagem: {}", event.travelId(), e);
+        } finally {
+            redisTrackingService.releaseStudentAwayStateLock(event.travelId());
+        }
+    }
+}
