411832281181ca4af0a40b105e21a7f7209ec5ba
Iago-on-github <Iago737@outlook.com>
2026-06-09 09:47:37 -0300

    docs(swagger): adiciona documentação do endpoint de 'signing' do AuthController.

--- src/main/java/com/travel_system/backend_app/controller/AuthController.java
+++ src/main/java/com/travel_system/backend_app/controller/AuthController.java
@@ -5,6 +5,11 @@ import com.travel_system.backend_app.model.dtos.request.LoginRequestDTO;
 import com.travel_system.backend_app.model.dtos.response.LoginResponseDTO;
 import com.travel_system.backend_app.model.dtos.response.RefreshTokenResponseDTO;
 import com.travel_system.backend_app.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -24,6 +29,22 @@ public class AuthController {
         this.tokenConfig = tokenConfig;
     }
 
+    @Operation(
+            summary = "Realiza Login no sistema.",
+            description = "Autentica um usuário com e-mail e senha.\n" +
+                    "Retorna os tokens de acesso (Access Token) e de atualização (Refresh Token). \n" +
+                    "**Rota pública (não requer token no cabeçalho).**",
+            tags = {"Authentication"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dados válidos no sistema, token gerado e autenticação permitida.",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = LoginResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida. Os dados enviados não estão no formato correto ou violam validações básicas.",
+                    content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "401", description = "Email ou senha inválidos no sistema.", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado no sistema", content = @Content(schema = @Schema(hidden = true))),
+
+    })
     @PostMapping("/signing")
     public ResponseEntity<LoginResponseDTO> signing(@Valid @RequestBody LoginRequestDTO data) {
         var token = authService.signing(data);
