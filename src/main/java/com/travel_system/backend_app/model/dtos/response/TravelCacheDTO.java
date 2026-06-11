e4dc77d507f13cb8a6bc81ed18afbbee9b4f1900
Iago-on-github <Iago737@outlook.com>
2026-06-11 17:46:57 -0300

    feat: - adição de novo DTO para cache estático das viagens, que será usado em fluxos de alta ingestão de dados

---
+++ src/main/java/com/travel_system/backend_app/model/dtos/response/TravelCacheDTO.java
@@ -0,0 +1,15 @@
+package com.travel_system.backend_app.model.dtos.response;
+
+import com.travel_system.backend_app.model.enums.TravelStatus;
+
+import java.util.UUID;
+
+public record TravelCacheDTO(UUID travelId,
+                             UUID cityId,
+                             TravelStatus travelStatus,
+                             Double finalLatitude,
+                             Double finalLongitude,
+                             String polylineRoute,
+                             Double distance,
+                             Double duration) {
+}
