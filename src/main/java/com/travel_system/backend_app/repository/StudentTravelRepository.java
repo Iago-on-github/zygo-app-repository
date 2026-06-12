4c392fa27fd34c986089713beefaf28823d0ded8
Iago-on-github <Iago737@outlook.com>
2026-06-12 19:07:38 -0300

    fix/feat: adição de nova consulta para buscar os estudantes com base em campos específicos usados no algoritmo de distance. - adicionado método de update de status do studentTravel

--- src/main/java/com/travel_system/backend_app/repository/StudentTravelRepository.java
+++ src/main/java/com/travel_system/backend_app/repository/StudentTravelRepository.java
@@ -1,8 +1,14 @@
 package com.travel_system.backend_app.repository;
 
 import com.travel_system.backend_app.model.StudentTravel;
+import com.travel_system.backend_app.model.dtos.StudentAwayStateDTO;
+import com.travel_system.backend_app.model.dtos.response.StudentTravelResponseDTO;
+import com.travel_system.backend_app.model.enums.StudentTravelStatus;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +17,25 @@ import java.util.UUID;
 
 @Repository
 public interface StudentTravelRepository extends JpaRepository<StudentTravel, UUID> {
+
+    @Query("""
+            SELECT new com.travel_system.backend_app.model.dtos.StudentAwayStateDTO(
+                st.id,
+                st.student.id,
+                st.student.email,
+                st.studentTravelStatus,
+                st.embark
+            )
+            FROM StudentTravel st
+            WHERE st.travel.id = :travelId
+        """)
+    List<StudentAwayStateDTO> findStudentsForAwayState(@Param("travelId") UUID travelId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE StudentTravel st SET st.studentTravelStatus = :status WHERE st.id = :studentTravelId")
+    void updateStudentTravelStatus(@Param("studentTravelId") UUID studentTravelId, @Param("status") StudentTravelStatus status);
+
     Optional<StudentTravel> findByStudentIdAndTravelId(UUID studentId, UUID travelId);
 
     Optional<StudentTravel> findByTravelIdAndStudentId(UUID id, UUID studentId);
