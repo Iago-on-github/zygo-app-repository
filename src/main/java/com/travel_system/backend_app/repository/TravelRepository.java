3e2334011f6372b92aef290584ee4f80f6e98c21
Iago-on-github <Iago737@outlook.com>
2026-06-12 19:06:00 -0300

    fix: ajuste na query jpql para buscar dados específicos ao invés da entidade completa, trocado entiade por dto

--- src/main/java/com/travel_system/backend_app/repository/TravelRepository.java
+++ src/main/java/com/travel_system/backend_app/repository/TravelRepository.java
@@ -1,6 +1,7 @@
 package com.travel_system.backend_app.repository;
 
 import com.travel_system.backend_app.model.Travel;
+import com.travel_system.backend_app.model.dtos.StudentTrackingPositionDTO;
 import com.travel_system.backend_app.model.enums.TravelStatus;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,13 +10,26 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface TravelRepository extends JpaRepository<Travel, UUID> {
 
-    @Query("SELECT t FROM Travel t LEFT JOIN FETCH t.studentTravels WHERE t.id = :id")
-    Optional<Travel> findByIdWithStudents(@Param("id") UUID id);
+        @Query("""
+        SELECT new com.travel_system.backend_app.model.dtos.StudentTrackingPositionDTO(
+            s.id,
+            p.latitude,
+            p.longitude
+        )
+        FROM StudentTravel st
+        JOIN st.student s
+        JOIN st.position p
+        WHERE st.travel.id = :travelId
+    """)
+        Set<StudentTrackingPositionDTO> findTrackingPositionsByTravelId(
+            @Param("travelId") UUID travelId
+    );
 
     boolean existsByIdAndTravelStatus(UUID travelId, TravelStatus travelStatus);
 
