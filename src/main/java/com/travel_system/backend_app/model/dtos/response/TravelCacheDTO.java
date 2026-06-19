ab66e42d17f9dc90cd708407c8b9c913f9dcf8dc
Iago-on-github <Iago737@outlook.com>
2026-06-19 08:14:25 -0300

    fix: removido parâmetro cityId

--- src/main/java/com/travel_system/backend_app/model/dtos/response/TravelCacheDTO.java
+++ src/main/java/com/travel_system/backend_app/model/dtos/response/TravelCacheDTO.java
@@ -5,7 +5,6 @@ import com.travel_system.backend_app.model.enums.TravelStatus;
 import java.util.UUID;
 
 public record TravelCacheDTO(UUID travelId,
-                             UUID cityId,
                              TravelStatus travelStatus,
                              Double finalLatitude,
                              Double finalLongitude,
