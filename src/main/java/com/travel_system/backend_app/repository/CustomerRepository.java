daf221e4ac84adf0d190db82e273c3e26695af96
Iago-on-github <Iago737@outlook.com>
2026-06-19 08:11:24 -0300

    feat: adição de repository

---
+++ src/main/java/com/travel_system/backend_app/repository/CustomerRepository.java
@@ -0,0 +1,23 @@
+package com.travel_system.backend_app.repository;
+
+import com.travel_system.backend_app.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+
+    List<Customer> findAllByActive(Boolean active);
+
+    Optional<Customer> findByCnpj(@Param("cnpj") String cnpj);
+
+    Optional<Customer> findBySlug(String slug);
+}
