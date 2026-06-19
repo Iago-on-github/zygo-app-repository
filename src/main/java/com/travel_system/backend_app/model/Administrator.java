cec39e9bcbf438065dfee0e7a9eaa9f739d6c6b5
Iago-on-github <Iago737@outlook.com>
2026-06-19 08:09:00 -0300

    feat: vínculo de Customer no constructor da super classe

--- src/main/java/com/travel_system/backend_app/model/Administrator.java
+++ src/main/java/com/travel_system/backend_app/model/Administrator.java
@@ -18,8 +18,8 @@ public class Administrator extends UserModel {
     public Administrator() {
     }
 
-    public Administrator(UUID id, String email, String password, String name, String lastName, String telephone, String profilePicture, LocalDateTime createdAt, LocalDateTime updatedAt, String cpf, String birthDate) {
-        super(id, email, password, name, lastName, telephone, profilePicture, GeneralStatus.ACTIVE, createdAt, updatedAt);
+    public Administrator(UUID id, String email, String password, String name, String lastName, String telephone, String profilePicture, GeneralStatus status, LocalDateTime createdAt, LocalDateTime updatedAt, Customer customer, String cpf, String birthDate) {
+        super(id, email, password, name, lastName, telephone, profilePicture, status, createdAt, updatedAt, customer);
         this.cpf = cpf;
         this.birthDate = birthDate;
     }
