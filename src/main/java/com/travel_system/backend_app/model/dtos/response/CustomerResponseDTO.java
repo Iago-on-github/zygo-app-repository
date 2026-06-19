dbef3b1759c6d94b874827dcb34dbf67daf7311d
Iago-on-github <Iago737@outlook.com>
2026-06-19 08:11:52 -0300

    feat: adição de dto de response para customer

---
+++ src/main/java/com/travel_system/backend_app/model/dtos/response/CustomerResponseDTO.java
@@ -0,0 +1,10 @@
+package com.travel_system.backend_app.model.dtos.response;
+
+import com.travel_system.backend_app.model.City;
+import com.travel_system.backend_app.model.enums.ClientSector;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record CustomerResponseDTO(UUID id, String name, String slug, String cnpj, boolean active, City city, ClientSector clientSector, String profilePicture, Instant createdAt) {
+}
