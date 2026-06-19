7198088f594d4017771fb5163200c823553eea07
Iago-on-github <Iago737@outlook.com>
2026-06-19 08:11:40 -0300

    feat: adição de dto de request para customer

---
+++ src/main/java/com/travel_system/backend_app/model/dtos/request/CustomerRequestDTO.java
@@ -0,0 +1,25 @@
+package com.travel_system.backend_app.model.dtos.request;
+
+import com.travel_system.backend_app.model.City;
+import com.travel_system.backend_app.model.UserModel;
+import com.travel_system.backend_app.model.enums.ClientSector;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+import java.util.UUID;
+
+public record CustomerRequestDTO(
+        @NotNull(message = "name is required")
+        String name,
+        @NotNull(message = "slug is required")
+        String slug,
+        @NotNull(message = "cnpj is required")
+        String cnpj,
+        @NotNull(message = "cityId is required")
+        UUID cityId,
+        Set<UserModel> users,
+        @NotNull(message = "clientSector is required")
+        ClientSector clientSector,
+        String profilePicture) {
+}
