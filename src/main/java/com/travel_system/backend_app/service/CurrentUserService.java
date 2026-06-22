5128136e06fc0d528bf717fd19f15e9354c92e5c
Iago-on-github <Iago737@outlook.com>
2026-06-22 10:36:44 -0300

    feat: criação de novo service para tratar de métodos que envolvem o contexto de autorização

---
+++ src/main/java/com/travel_system/backend_app/service/CurrentUserService.java
@@ -0,0 +1,16 @@
+package com.travel_system.backend_app.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CurrentUserService {
+
+    // verifica se o adm logado é o adm da plataforma
+    protected boolean isPlatformAdmin() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        return auth.getAuthorities().stream().anyMatch(p -> p.getAuthority().equals("ROLE_PLATFORM_ADMIN"));
+    }
+}
