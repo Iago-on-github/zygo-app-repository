14ffb5f1a0b0ca00e6153674b77872f6ae734818
Iago-on-github <Iago737@outlook.com>
2026-06-22 10:44:20 -0300

    feat: adição de novo método para buscar usermodel por email e role

--- src/main/java/com/travel_system/backend_app/repository/UserRepository.java
+++ src/main/java/com/travel_system/backend_app/repository/UserRepository.java
@@ -5,6 +5,7 @@ import com.travel_system.backend_app.model.enums.GeneralStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -22,4 +23,7 @@ public interface UserRepository extends JpaRepository<UserModel, UUID> {
     boolean existsByEmailAndIdAndStatus(String email, UUID id, GeneralStatus status);
 
     Optional<UserModel> findByEmailAndCustomerId(String email, UUID customerId);
+
+    @Query("SELECT u FROM UserModel u JOIN u.permissions p WHERE u.email = :email AND p.description =: role")
+    Optional<UserModel> findByEmailAndRole(@Param("email") String email, @Param("role") String role);
 }
