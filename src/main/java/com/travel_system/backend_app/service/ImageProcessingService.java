a74c7c5388ce0252956c8a533aab132659e3a0eb
Iago-on-github <Iago737@outlook.com>
2026-06-29 17:36:58 -0300

    fix: método convert readaptado para converter nativamente todos os tipos de entrada para JPEG, retirando plugins externos do 12monkeys e usando estratégias para evitar fundo transparente bugado (em casos de PNG)

--- src/main/java/com/travel_system/backend_app/service/ImageProcessingService.java
+++ src/main/java/com/travel_system/backend_app/service/ImageProcessingService.java
@@ -4,35 +4,39 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-/*
-* utiliza 12monkeys para converter imagens para tipos que o ImageIO do JDK não suporta nativamente
-* */
-
 @Service
 public class ImageProcessingService {
 
-    public byte[] convertImageToWebp(MultipartFile file) throws IOException {
+    public byte[] convertImageToJPEG(MultipartFile file) throws IOException {
         if (file == null || file.isEmpty()) {
-            throw new IllegalArgumentException("[convertImageToWebp] file não pode estar null ou inválido");
+            throw new IllegalArgumentException("[convertImageToJPEG] file não pode estar null ou inválido");
         }
 
-        if (!isImage(file)) throw new IllegalArgumentException("[convertImageToWebp] não é uma imagem");
+        if (!isImage(file)) throw new IllegalArgumentException("[convertImageToJPEG] não é uma imagem");
 
         try (InputStream inputStream = file.getInputStream()) {
-            BufferedImage image = ImageIO.read(inputStream);
+            BufferedImage originalImage = ImageIO.read(inputStream);
 
-            if (image == null) throw new IOException("[convertImageToWebp] image null");
+            if (originalImage == null) throw new IOException("[convertImageToJPEG] originalImage null");
+
+            BufferedImage rgbImage = new BufferedImage(
+                    originalImage.getWidth(),
+                    originalImage.getHeight(),
+                    BufferedImage.TYPE_INT_RGB
+            );
+
+            Graphics2D graphics2D = rgbImage.createGraphics();
+            graphics2D.drawImage(originalImage, 0, 0, Color.white, null);
+            graphics2D.dispose();
 
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-            boolean converted = ImageIO.write(image, "webp", outputStream);
-
-            if (!converted) throw new IOException("writer WebP encontrado");
+            ImageIO.write(rgbImage, "jpeg", outputStream);
 
             return outputStream.toByteArray();
         }
