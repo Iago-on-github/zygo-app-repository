810c6ad95a7cd629f902c562ee3dc6a5d8da13d1
Iago-on-github <Iago737@outlook.com>
2026-07-02 12:21:42 -0300

    feat: adição de nova propriedade "travelPeriod"

--- src/main/java/com/travel_system/backend_app/model/dtos/request/TravelRequestDTO.java
+++ src/main/java/com/travel_system/backend_app/model/dtos/request/TravelRequestDTO.java
@@ -2,10 +2,13 @@ package com.travel_system.backend_app.model.dtos.request;
 
 
 
+import com.travel_system.backend_app.model.enums.TravelPeriod;
+
 import java.util.UUID;
 
 public record TravelRequestDTO(
         UUID driverId,
+        TravelPeriod travelPeriod,
         Double originLongitude,
         Double originLatitude,
         Double finalLongitude,
