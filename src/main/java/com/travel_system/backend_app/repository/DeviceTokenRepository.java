c41cbfec51228338e6754dd161bf932e1904b3d0
Iago-on-github <Iago737@outlook.com>
2026-07-02 12:20:50 -0300

    feat: adição de novos métodos no repository deviceToken atrelado ao firebase - métodos buscam os tokens ativos de cada usuário

--- src/main/java/com/travel_system/backend_app/repository/DeviceTokenRepository.java
+++ src/main/java/com/travel_system/backend_app/repository/DeviceTokenRepository.java
@@ -19,4 +19,23 @@ public interface DeviceTokenRepository extends JpaRepository<DeviceToken, UUID> 
     @Modifying // Indica que é uma operação de escrita (UPDATE/DELETE)
     @Query("UPDATE DeviceToken dt SET dt.active = false WHERE dt.token IN :tokens")
     void deactivateTokensByValue(@Param("tokens") List<String> tokens);
+
+    @Query("SELECT dt.token FROM DeviceToken dt WHERE dt.user.id= :userId AND dt.active = TRUE")
+    Set<String> findTokensByUserId(@Param("userId") UUID userId);
+
+    @Query("SELECT dt.token FROM DeviceToken dt WHERE dt.user.customer.id = :customerId AND dt.active = TRUE")
+    Set<String> findTokensByCustomerId(@Param("customerId") UUID customerId);
+
+    // DISTINCT evita duplicidade de tokens caso o user tenha mais de uma ROLE
+    @Query("SELECT DISTINCT dt.token FROM DeviceToken dt JOIN dt.user.permissions p WHERE dt.user.customer.id = :customerId AND p.description = :role AND dt.active = TRUE")
+    Set<String> findTokensByCustomerIdAndUserType(@Param("customerId") UUID customerId, @Param("role") String role);
+
+    @Query("SELECT DISTINCT dt.token FROM DeviceToken dt JOIN dt.user.permissions p WHERE dt.user.customer.id = :customerId AND p.description IN :roles AND dt.active = TRUE")
+    Set<String> findActiveTokensByCustomerIdAndRoles(@Param("customerId") UUID customerId, @Param("roles") List<String> roles);
+
+    @Query("SELECT DISTINCT dt.token FROM Travel t JOIN t.studentTravels st JOIN st.student s JOIN DeviceToken dt ON dt.user = s WHERE t.id = :travelId AND dt.active = TRUE")
+    Set<String> findActiveTokensByTravelId(@Param("travelId") UUID travelId);
+
+    @Query("SELECT DISTINCT dt.token FROM Travel t JOIN t.studentTravels st JOIN st.student s JOIN DeviceToken dt ON dt.user = s WHERE t.id = :travelId AND dt.active = TRUE AND st.embark = TRUE")
+    Set<String> findActiveTokensByTravelIdAndEmbarkTrue(@Param("travelId") UUID travelId);
 }
