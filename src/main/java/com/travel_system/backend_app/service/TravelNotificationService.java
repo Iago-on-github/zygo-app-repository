28dfbb684ed74c2856602d399bdc40ad80d0f795
Iago-on-github <Iago737@outlook.com>
2026-07-03 08:23:33 -0300

    feat: adição de notificação para cancelamento da viagem

--- src/main/java/com/travel_system/backend_app/service/TravelNotificationService.java
+++ src/main/java/com/travel_system/backend_app/service/TravelNotificationService.java
@@ -117,4 +117,35 @@ public class TravelNotificationService {
         firebaseNotificationSender.sendPushNotification(driver);
         firebaseNotificationSender.sendPushNotification(admin);
     }
+
+    // CANCELAMENTO DE VIAGEM
+    public void sendTravelCanceledNotification(Travel travel) {
+        NotificationAudience travelStudents = NotificationAudience.TRAVEL_STUDENTS;
+        NotificationAudience driverNotification = NotificationAudience.SPECIFIC_USER; // envia apenas para o motorista da viagem
+        NotificationAudience adminNotification = NotificationAudience.ADMIN_ONLY;
+
+        String title = "Viagem cancelada";
+        String message = "A viagem do turno" + travel.getTravelPeriod() + "foi cancelada";
+        String link = "/travels/" + travel.getId() + "/tracking";
+
+        Map<String, String> data = Map.of(
+                "eventType", "TRAVEL_CANCELED",
+                "travelId", travel.getId().toString(),
+                "period", travel.getTravelPeriod().toString()
+        );
+
+        // students
+        PushNotificationCommandDTO students = new PushNotificationCommandDTO(travelStudents, null, null, travel.getId(), title, message, link, Priority.NORMAL, data);
+
+        // driver
+        PushNotificationCommandDTO driver = new PushNotificationCommandDTO(driverNotification, travel.getDriver().getId(), null, travel.getId(), title, message, link, Priority.NORMAL, data);
+
+        // admins
+        PushNotificationCommandDTO admin = new PushNotificationCommandDTO(adminNotification, null, travel.getDriver().getCustomer().getId(), travel.getId(), title, message, link, Priority.NORMAL, data);
+
+        // envia as notificações ao firebase
+        firebaseNotificationSender.sendPushNotification(students);
+        firebaseNotificationSender.sendPushNotification(driver);
+        firebaseNotificationSender.sendPushNotification(admin);
+    }
 }
