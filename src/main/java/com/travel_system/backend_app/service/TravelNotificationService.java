e9641c61ebd780a70c2d5020fb2a7d27bc238146
Iago-on-github <Iago737@outlook.com>
2026-07-03 11:18:23 -0300

    feat/fix: notificações estáticas da viagem - [feat]: adição de notificação para mudança de motorista - [fix]: refatoração nos métodos existentes para adição de método handler.

--- src/main/java/com/travel_system/backend_app/service/TravelNotificationService.java
+++ src/main/java/com/travel_system/backend_app/service/TravelNotificationService.java
@@ -1,5 +1,6 @@
 package com.travel_system.backend_app.service;
 
+import com.travel_system.backend_app.model.Driver;
 import com.travel_system.backend_app.model.Travel;
 import com.travel_system.backend_app.model.dtos.notifications.PushNotificationCommandDTO;
 import com.travel_system.backend_app.model.enums.NotificationAudience;
@@ -9,6 +10,7 @@ import com.travel_system.backend_app.utils.FirebaseNotificationSender;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.UUID;
 
 /*
 * notificações estáticas da viagem
@@ -27,10 +29,6 @@ public class TravelNotificationService {
 
     // CREATE TRAVEL
     public void sendTravelCreatedNotification(Travel travel) {
-        NotificationAudience travelStudents = NotificationAudience.TRAVEL_STUDENTS;
-        NotificationAudience driverNotification = NotificationAudience.SPECIFIC_USER; // envia apenas para o motorista da viagem
-        NotificationAudience adminNotification = NotificationAudience.ADMIN_ONLY;
-
         String title = "Nova viagem criada";
         String message = "Uma nova viagem para o turno" + travel.getTravelPeriod() + "foi criada";
         String link = "/travels/" + travel.getId() + "/tracking";
@@ -41,27 +39,12 @@ public class TravelNotificationService {
                 "period", travel.getTravelPeriod().toString()
         );
 
-        // students
-        PushNotificationCommandDTO students = new PushNotificationCommandDTO(travelStudents, null, null, travel.getId(), title, message, link, Priority.NORMAL, data);
-
-        // driver
-        PushNotificationCommandDTO driver = new PushNotificationCommandDTO(driverNotification, travel.getDriver().getId(), null, travel.getId(), title, message, link, Priority.NORMAL, data);
-
-        // admins
-        PushNotificationCommandDTO admin = new PushNotificationCommandDTO(adminNotification, null, travel.getDriver().getCustomer().getId(), travel.getId(), title, message, link, Priority.NORMAL, data);
-
-        // envia as notificações ao firebase
-        firebaseNotificationSender.sendPushNotification(students);
-        firebaseNotificationSender.sendPushNotification(driver);
-        firebaseNotificationSender.sendPushNotification(admin);
+        // metodo handler auxiliar
+        handleTravelNotification(travel, title, message, link, data);
     }
 
     // INICÍO DE VIAGEM
     public void sendTravelStartedNotification(Travel travel) {
-        NotificationAudience travelStudents = NotificationAudience.TRAVEL_STUDENTS;
-        NotificationAudience driverNotification = NotificationAudience.SPECIFIC_USER; // envia apenas para o motorista da viagem
-        NotificationAudience adminNotification = NotificationAudience.ADMIN_ONLY;
-
         String title = "Viagem iniciada";
         String message = "A viagem do turno" + travel.getTravelPeriod() + "foi iniciada";
         String link = "/travels/" + travel.getId() + "/tracking";
@@ -72,27 +55,12 @@ public class TravelNotificationService {
                 "period", travel.getTravelPeriod().toString()
         );
 
-        // students
-        PushNotificationCommandDTO students = new PushNotificationCommandDTO(travelStudents, null, null, travel.getId(), title, message, link, Priority.NORMAL, data);
-
-        // driver
-        PushNotificationCommandDTO driver = new PushNotificationCommandDTO(driverNotification, travel.getDriver().getId(), null, travel.getId(), title, message, link, Priority.NORMAL, data);
-
-        // admins
-        PushNotificationCommandDTO admin = new PushNotificationCommandDTO(adminNotification, null, travel.getDriver().getCustomer().getId(), travel.getId(), title, message, link, Priority.NORMAL, data);
-
-        // envia as notificações ao firebase
-        firebaseNotificationSender.sendPushNotification(students);
-        firebaseNotificationSender.sendPushNotification(driver);
-        firebaseNotificationSender.sendPushNotification(admin);
+        // metodo handler auxiliar
+        handleTravelNotification(travel, title, message, link, data);
     }
 
     // FIM DE VIAGEM
     public void sendTravelEndedNotification(Travel travel) {
-        NotificationAudience travelStudents = NotificationAudience.TRAVEL_STUDENTS;
-        NotificationAudience driverNotification = NotificationAudience.SPECIFIC_USER; // envia apenas para o motorista da viagem
-        NotificationAudience adminNotification = NotificationAudience.ADMIN_ONLY;
-
         String title = "Viagem finalizada";
         String message = "A viagem do turno" + travel.getTravelPeriod() + "foi finalizada";
         String link = "/travels/" + travel.getId() + "/tracking";
@@ -103,27 +71,12 @@ public class TravelNotificationService {
                 "period", travel.getTravelPeriod().toString()
         );
 
-        // students
-        PushNotificationCommandDTO students = new PushNotificationCommandDTO(travelStudents, null, null, travel.getId(), title, message, link, Priority.NORMAL, data);
-
-        // driver
-        PushNotificationCommandDTO driver = new PushNotificationCommandDTO(driverNotification, travel.getDriver().getId(), null, travel.getId(), title, message, link, Priority.NORMAL, data);
-
-        // admins
-        PushNotificationCommandDTO admin = new PushNotificationCommandDTO(adminNotification, null, travel.getDriver().getCustomer().getId(), travel.getId(), title, message, link, Priority.NORMAL, data);
-
-        // envia as notificações ao firebase
-        firebaseNotificationSender.sendPushNotification(students);
-        firebaseNotificationSender.sendPushNotification(driver);
-        firebaseNotificationSender.sendPushNotification(admin);
+        // metodo handler auxiliar
+        handleTravelNotification(travel, title, message, link, data);
     }
 
     // CANCELAMENTO DE VIAGEM
     public void sendTravelCanceledNotification(Travel travel) {
-        NotificationAudience travelStudents = NotificationAudience.TRAVEL_STUDENTS;
-        NotificationAudience driverNotification = NotificationAudience.SPECIFIC_USER; // envia apenas para o motorista da viagem
-        NotificationAudience adminNotification = NotificationAudience.ADMIN_ONLY;
-
         String title = "Viagem cancelada";
         String message = "A viagem do turno" + travel.getTravelPeriod() + "foi cancelada";
         String link = "/travels/" + travel.getId() + "/tracking";
@@ -134,16 +87,64 @@ public class TravelNotificationService {
                 "period", travel.getTravelPeriod().toString()
         );
 
+        // metodo handler auxiliar
+        handleTravelNotification(travel, title, message, link, data);
+    }
+
+    // MUDANÇA DE MOTORISTA
+    public void sendDriverChangedNotification(Travel travel, Driver newDriver) {
+        NotificationAudience travelStudents = NotificationAudience.TRAVEL_STUDENTS;
+        NotificationAudience driverNotification = NotificationAudience.SPECIFIC_USER; // envia apenas para o novo motorista da viagem
+
+        String title = "Mudança de motorista";
+        String message = "Houve uma modificação no motorista da viagem do turno" + travel.getTravelPeriod() + ". Novo motorista: " + newDriver.getName();
+        String link = "/travels/" + travel.getId() + "/tracking";
+
+        Map<String, String> data = Map.of(
+                "eventType", "DRIVER_CHANGED",
+                "travelId", travel.getId().toString(),
+                "period", travel.getTravelPeriod().toString()
+        );
+
         // students
         PushNotificationCommandDTO students = new PushNotificationCommandDTO(travelStudents, null, null, travel.getId(), title, message, link, Priority.NORMAL, data);
 
         // driver
-        PushNotificationCommandDTO driver = new PushNotificationCommandDTO(driverNotification, travel.getDriver().getId(), null, travel.getId(), title, message, link, Priority.NORMAL, data);
-
-        // admins
-        PushNotificationCommandDTO admin = new PushNotificationCommandDTO(adminNotification, null, travel.getDriver().getCustomer().getId(), travel.getId(), title, message, link, Priority.NORMAL, data);
+        PushNotificationCommandDTO driver = new PushNotificationCommandDTO(driverNotification, newDriver.getId(), null, travel.getId(), title, message, link, Priority.NORMAL, data);
 
         // envia as notificações ao firebase
+        firebaseNotificationSender.sendPushNotification(students);
+        firebaseNotificationSender.sendPushNotification(driver);
+    }
+
+    // timeable change
+
+    // HANDLER
+    private void handleTravelNotification(Travel travel, String title, String message, String link, Map<String, String> data) {
+        NotificationAudience travelStudents = NotificationAudience.TRAVEL_STUDENTS;
+        NotificationAudience driverNotification = NotificationAudience.SPECIFIC_USER; // envia apenas para o motorista da viagem
+        NotificationAudience adminNotification = NotificationAudience.ADMIN_ONLY;
+
+        UUID travelId = travel.getId();
+        UUID driverId = travel.getDriver().getId();
+        UUID customerId = travel.getDriver().getCustomer().getId();
+
+        // notificação estudantes
+        PushNotificationCommandDTO students = new PushNotificationCommandDTO(
+                travelStudents, null, null, travelId, title, message, link, Priority.NORMAL, data
+        );
+
+        // Notificação motorista
+        PushNotificationCommandDTO driver = new PushNotificationCommandDTO(
+                driverNotification, driverId, null, travelId, title, message, link, Priority.NORMAL, data
+        );
+
+        // Notificação administradores
+        PushNotificationCommandDTO admin = new PushNotificationCommandDTO(
+                adminNotification, null, customerId, travelId, title, message, link, Priority.NORMAL, data
+        );
+
+        // envia todas as notificações
         firebaseNotificationSender.sendPushNotification(students);
         firebaseNotificationSender.sendPushNotification(driver);
         firebaseNotificationSender.sendPushNotification(admin);
