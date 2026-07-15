968159b891d201d0c1027f8a9b4cf4d68372892a
Iago-on-github <Iago737@outlook.com>
2026-07-15 08:57:07 -0300

    tests: adição de testes unitários para validar cenários de processamento dos dados e envio ao service do firebase

--- src/test/java/com/travel_system/backend_app/service/AsyncNotificationServiceTest.java
+++ src/test/java/com/travel_system/backend_app/service/AsyncNotificationServiceTest.java
@@ -1,11 +1,15 @@
 package com.travel_system.backend_app.service;
 
 import com.travel_system.backend_app.exceptions.EtaDataStatesInvalidException;
+import com.travel_system.backend_app.model.Travel;
 import com.travel_system.backend_app.model.dtos.VelocityAnalysisDTO;
 import com.travel_system.backend_app.model.enums.MovementState;
 import com.travel_system.backend_app.model.enums.ShouldNotify;
 import com.travel_system.backend_app.repository.StudentTravelRepository;
+import com.travel_system.backend_app.repository.TravelRepository;
 import com.travel_system.backend_app.utils.FirebaseNotificationSender;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -16,6 +20,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -39,172 +44,175 @@ class AsyncNotificationServiceTest {
 
     @Mock
     private RedisTrackingService redisTrackingService;
-
     @Mock
-    private FirebaseNotificationSender firebaseNotificationSender;
+    private TravelRepository travelRepository;
+    @Mock
+    private TravelTrackingNotificationService trackingNotificationService;
 
     @Mock
     private StudentTravelRepository studentTravelRepository;
+
+    private Travel travelEntity;
+    private UUID travelId;
+    private VelocityAnalysisDTO velocityAnalysisDTO;
+
+    @BeforeEach
+    void setUp() {
+        travelId = UUID.randomUUID();
+        travelEntity = new Travel();
+        travelEntity.setId(travelId);
+
+        velocityAnalysisDTO = new VelocityAnalysisDTO(85.5, 3600L, 85.5, 1718915200000.0, MovementState.NORMAL);
+    }
 
     @Nested
     class processNotificationType {
 
         @Test
-        @DisplayName("should process notification and no notify with success")
-        void shouldProcessNotificationAndNoNotifyWithSuccess() {
-            // arrange
-            UUID traceId = UUID.randomUUID();
-            UUID travelId = UUID.randomUUID();
-            UUID studentId = UUID.randomUUID();
+        @DisplayName("Deve retornar de forma silenciosa caso não seja requerida notificação")
+        void shouldReturnImmediatelyWhenNotificationIsNotRequired() {
+            asyncNotificationService.processNotificationType(travelId, velocityAnalysisDTO, ShouldNotify.SHOULD_NO_NOTIFY);
 
-            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(3.2, 4L, 8.0, 2.1, MovementState.NORMAL);
-
-            // act
-            asyncNotificationService.processNotificationType(travelId, velocityAnalysis, ShouldNotify.SHOULD_NO_NOTIFY, traceId);
-
-            // assert
-            verify(redisTrackingService, never()).markNotificationAsSent(travelId);
-
-            verify(studentTravelRepository, never()).findStudentIdsByTravelIdAndDisembarkHourIsNull(travelId);
-
-            verify(firebaseNotificationSender, never())
-                    .pushNotificationToFirebase(argThat(event -> event.studentId().equals(studentId) &&
-                            event.travelId().equals(travelId) &&
-                            event.movementState().equals(MovementState.SLOW) &&
-                            event.traceId().equals(traceId)));
+            verifyNoInteractions(travelRepository);
+            verifyNoInteractions(redisTrackingService);
+            verifyNoInteractions(trackingNotificationService);
         }
 
         @Test
-        @DisplayName("should process notification and returns slow notification with success")
-        void shouldProcessNotificationAndReturnsSlowNotificationWithSuccess() {
-            // arrange
-            UUID traceId = UUID.randomUUID();
-            UUID travelId = UUID.randomUUID();
-            UUID studentId = UUID.randomUUID();
+        @DisplayName("Cenário 1.2: Deve direcionar para fluxo de ônibus lento quando ShouldNotify for SHOULD_NOTIFY_SLOW")
+        void shouldRedirectToSlowNotificationWhenShouldNotifyIsSlow() {
+            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(85.5, 3600L, 85.5, 1718915200000.0, MovementState.SLOW);
 
-            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(3.2, 4L, 8.0, 2.1, MovementState.SLOW);
 
-            List<UUID> studentsId = List.of(studentId);
+            when(travelRepository.findById(travelId)).thenReturn(Optional.of(travelEntity));
 
-            when(studentTravelRepository.findStudentIdsByTravelIdAndDisembarkHourIsNull(travelId)).thenReturn(studentsId);
+            asyncNotificationService.processNotificationType(travelId, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_SLOW);
 
-            // act
-            asyncNotificationService.processNotificationType(travelId, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_SLOW, traceId);
-
-            // assert
+            // Verifica que o fluxo do SLOW foi executado por completo
+            verify(travelRepository, times(1)).findById(travelId);
             verify(redisTrackingService, times(1)).markNotificationAsSent(travelId);
+            verify(trackingNotificationService, times(1))
+                    .sendTrackingSlowMovementNotification(travelEntity, velocityAnalysis);
 
-            verify(studentTravelRepository, times(1)).findStudentIdsByTravelIdAndDisembarkHourIsNull(travelId);
-
-            verify(firebaseNotificationSender, times(1))
-                    .pushNotificationToFirebase(argThat(event -> event.studentId().equals(studentId) &&
-                            event.travelId().equals(travelId) &&
-                            event.movementState().equals(MovementState.SLOW) &&
-                            event.traceId().equals(traceId)));
-
+            verify(trackingNotificationService, never())
+                    .sendTrackingStoppedMovementNotification(any(), any());
         }
 
         @Test
-        @DisplayName("should process notification and returns stopped notification with success")
-        void shouldProcessNotificationAndReturnsStoppedNotificationWithSuccess() {
-            // arrange
-            UUID traceId = UUID.randomUUID();
-            UUID travelId = UUID.randomUUID();
-            UUID studentId = UUID.randomUUID();
+        @DisplayName("Cenário 1.3: Deve direcionar para fluxo de ônibus parado quando ShouldNotify for SHOULD_NOTIFY_STOPPED")
+        void shouldRedirectToStoppedNotificationWhenShouldNotifyIsStopped() {
+            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(85.5, 3600L, 85.5, 1718915200000.0, MovementState.STOPPED);
 
-            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(3.2, 4L, 8.0, 2.1, MovementState.STOPPED);
+            when(travelRepository.findById(travelId)).thenReturn(Optional.of(travelEntity));
 
-            List<UUID> studentsId = List.of(studentId);
+            asyncNotificationService.processNotificationType(travelId, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_STOPPED);
 
-            when(studentTravelRepository.findStudentIdsByTravelIdAndDisembarkHourIsNull(travelId)).thenReturn(studentsId);
-
-            // act
-            asyncNotificationService.processNotificationType(travelId, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_STOPPED, traceId);
-
-            // assert
+            // Verifica que o fluxo do STOPPED foi executado por completo
+            verify(travelRepository, times(1)).findById(travelId);
             verify(redisTrackingService, times(1)).markNotificationAsSent(travelId);
+            verify(trackingNotificationService, times(1))
+                    .sendTrackingStoppedMovementNotification(travelEntity, velocityAnalysis);
 
-            verify(studentTravelRepository, times(1)).findStudentIdsByTravelIdAndDisembarkHourIsNull(travelId);
-
-            verify(firebaseNotificationSender, times(1))
-                    .pushNotificationToFirebase(argThat(event -> event.studentId().equals(studentId) &&
-                            event.travelId().equals(travelId) &&
-                            event.movementState().equals(MovementState.STOPPED) &&
-                            event.traceId().equals(traceId)));
+            verify(trackingNotificationService, never())
+                    .sendTrackingSlowMovementNotification(any(), any());
         }
 
         @Test
-        @DisplayName("throw exception when travelId is null")
-        void throwExceptionWhenTravelIdIsNull() {
-            // arrange
-            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(3.2, 4L, 8.0, 2.1, MovementState.STOPPED);
-            UUID traceId = UUID.randomUUID();
+        @DisplayName("Cenário 2.1: Deve enviar notificação de lentidão com sucesso e registrar no Redis")
+        void shouldSendSlowNotificationWithSuccess() {
+            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(85.5, 3600L, 85.5, 1718915200000.0, MovementState.SLOW);
 
-            // act & assert
+
+            when(travelRepository.findById(travelId)).thenReturn(Optional.of(travelEntity));
+
+            asyncNotificationService.processNotificationType(travelId, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_SLOW);
+
+            verify(travelRepository, times(1)).findById(travelId);
+            verify(redisTrackingService, times(1)).markNotificationAsSent(travelId);
+            verify(trackingNotificationService, times(1))
+                    .sendTrackingSlowMovementNotification(travelEntity, velocityAnalysis);
+        }
+
+        @Test
+        @DisplayName("Cenário 2.2: Deve lançar EtaDataStatesInvalidException quando a análise de velocidade for nula")
+        void shouldThrowExceptionWhenVelocityAnalysisIsNull() {
             assertThrows(EtaDataStatesInvalidException.class, () -> {
-               asyncNotificationService.processNotificationType(null, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_SLOW, traceId);
+                asyncNotificationService.processNotificationType(travelId, null, ShouldNotify.SHOULD_NOTIFY_SLOW);
             });
 
-            verifyNoInteractions(redisTrackingService, studentTravelRepository, firebaseNotificationSender);
+            verifyNoInteractions(travelRepository, redisTrackingService, trackingNotificationService);
         }
 
         @Test
-        @DisplayName("throw exception when velocity analysis dto is null")
-        void throwExceptionWhenVelocityAnalysisIsNull() {
-            // arrange
-            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(3.2, 4L, 8.0, 2.1, MovementState.STOPPED);
-            UUID traceId = UUID.randomUUID();
-            UUID travelId = UUID.randomUUID();
+        @DisplayName("Cenário 2.4: Deve retornar silenciosamente sem enviar notificação quando o estado de movimento não for SLOW")
+        void shouldReturnSilentlyWhenMovementStateIsNotSlow() {
+            // Estado configurado como NORMAL ao invés de SLOW
+            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(85.5, 3600L, 85.5, 1718915200000.0, MovementState.NORMAL);
 
-            // act & assert
-            assertThrows(EtaDataStatesInvalidException.class, () -> {
-                asyncNotificationService.processNotificationType(travelId, null, ShouldNotify.SHOULD_NOTIFY_SLOW, traceId);
-            });
+            when(travelRepository.findById(travelId)).thenReturn(Optional.of(travelEntity));
 
-            verifyNoInteractions(redisTrackingService, studentTravelRepository, firebaseNotificationSender);
+            asyncNotificationService.processNotificationType(travelId, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_SLOW);
+
+            verify(travelRepository, times(1)).findById(travelId);
+            // Garante que o registro no Redis e o envio no Firebase não foram chamados devido ao early return do estado incompatível
+            verifyNoInteractions(redisTrackingService, trackingNotificationService);
         }
 
         @Test
-        @DisplayName("should returns if movement state are inconsistent with should notify")
-        void shouldReturnsIfMovementStateAreInconsistentWithShouldNotify() {
-            // arrange
-            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(3.2, 4L, 8.0, 2.1, MovementState.STOPPED);
-            UUID traceId = UUID.randomUUID();
-            UUID travelId = UUID.randomUUID();
+        @DisplayName("Cenário 3.1: Deve enviar notificação de veículo parado com sucesso e registrar no Redis")
+        void shouldSendStoppedNotificationWithSuccess() {
+            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(85.5, 3600L, 85.5, 1718915200000.0, MovementState.STOPPED);
 
-            // act
-            asyncNotificationService.processNotificationType(travelId, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_SLOW, traceId);
+            when(travelRepository.findById(travelId)).thenReturn(Optional.of(travelEntity));
 
-            // assert
-            verifyNoInteractions(redisTrackingService, studentTravelRepository, firebaseNotificationSender);
+            asyncNotificationService.processNotificationType(travelId, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_STOPPED);
+
+            verify(travelRepository, times(1)).findById(travelId);
+            verify(redisTrackingService, times(1)).markNotificationAsSent(travelId);
+            verify(trackingNotificationService, times(1))
+                    .sendTrackingStoppedMovementNotification(travelEntity, velocityAnalysis);
         }
 
         @Test
-        @DisplayName("it should throw an exception when Firebase crashes and not interrupt any other services.")
-        void shouldThrowAnExceptionWhenFirebaseFailsAndNotInterruptAnyOtherElseSenders() {
-            // arrange
-            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(3.2, 4L, 8.0, 2.1, MovementState.SLOW);
-            UUID traceId = UUID.randomUUID();
-            UUID travelId = UUID.randomUUID();
+        @DisplayName("Cenário 3.2: Deve lançar EtaDataStatesInvalidException quando algum parâmetro obrigatório for nulo")
+        void shouldThrowExceptionWhenRequiredParametersAreNull() {
+            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(85.5, 3600L, 85.5, 1718915200000.0, MovementState.STOPPED);
 
-            UUID studentIdThatFails = UUID.randomUUID();
-            UUID studentIdThatSucceeds = UUID.randomUUID();
+            // Testando com travelId nulo
+            assertThrows(EtaDataStatesInvalidException.class, () -> asyncNotificationService.processNotificationType(null, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_STOPPED));
 
-            List<UUID> studentList = List.of(studentIdThatFails, studentIdThatSucceeds);
+            // Testando com velocityAnalysis nulo
+            assertThrows(EtaDataStatesInvalidException.class, () -> asyncNotificationService.processNotificationType(travelId, null, ShouldNotify.SHOULD_NOTIFY_STOPPED));
 
-            when(studentTravelRepository.findStudentIdsByTravelIdAndDisembarkHourIsNull(travelId)).thenReturn(studentList);
+            verifyNoInteractions(travelRepository, redisTrackingService, trackingNotificationService);
+        }
 
-            // throw exception to first student
-            doThrow(new RuntimeException("Firebase unavailable"))
-                    .when(firebaseNotificationSender)
-                    .pushNotificationToFirebase(argThat(event -> event.studentId().equals(studentIdThatFails)));
+        @Test
+        @DisplayName("Cenário 3.3: Deve lançar EntityNotFoundException quando a viagem não for encontrada no banco de dados")
+        void shouldThrowEntityNotFoundExceptionWhenTravelNotFound() {
+            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(85.5, 3600L, 85.5, 1718915200000.0, MovementState.STOPPED);
 
-            // assert
-            assertDoesNotThrow(() -> {
-                asyncNotificationService.processNotificationType(travelId, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_SLOW, traceId);
-            });
+            when(travelRepository.findById(travelId)).thenReturn(Optional.empty());
 
-            verify(firebaseNotificationSender, times(2)).pushNotificationToFirebase(any());
+            assertThrows(EntityNotFoundException.class, () -> asyncNotificationService.processNotificationType(travelId, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_STOPPED));
+
+            verify(travelRepository, times(1)).findById(travelId);
+            verifyNoInteractions(redisTrackingService, trackingNotificationService);
+        }
+
+        @Test
+        @DisplayName("Cenário 3.4: Deve retornar silenciosamente sem enviar notificação quando o estado de movimento não for STOPPED")
+        void shouldReturnSilentlyWhenMovementStateIsNotStopped() {
+            // Estado configurado como NORMAL ao invés de STOPPED
+            VelocityAnalysisDTO velocityAnalysis = new VelocityAnalysisDTO(85.5, 3600L, 85.5, 1718915200000.0, MovementState.NORMAL);
+
+            when(travelRepository.findById(travelId)).thenReturn(Optional.of(travelEntity));
+
+            asyncNotificationService.processNotificationType(travelId, velocityAnalysis, ShouldNotify.SHOULD_NOTIFY_STOPPED);
+
+            verify(travelRepository, times(1)).findById(travelId);
+            // Garante que o registro no Redis e o envio no Firebase não foram chamados devido ao early return do estado incompatível
+            verifyNoInteractions(redisTrackingService, trackingNotificationService);
         }
     }
 }
