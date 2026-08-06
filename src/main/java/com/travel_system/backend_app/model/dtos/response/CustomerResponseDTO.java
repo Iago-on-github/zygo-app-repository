c638b184408b1ffde91587f87e71ef3a8f4ddd98
Iago-on-github <Iago737@outlook.com>
2026-08-06 09:24:08 -0300

    fix: reorganization

--- src/main/java/com/travel_system/backend_app/model/dtos/response/CustomerResponseDTO.java
+++ src/main/java/com/travel_system/backend_app/model/dtos/response/CustomerResponseDTO.java
@@ -6,5 +6,13 @@ import com.travel_system.backend_app.model.enums.ClientSector;
 import java.time.Instant;
 import java.util.UUID;
 
-public record CustomerResponseDTO(UUID id, String name, String slug, String cnpj, boolean active, City city, ClientSector clientSector, String profilePicture, Instant createdAt) {
+public record CustomerResponseDTO(UUID id,
+                                  String name,
+                                  String slug,
+                                  String cnpj,
+                                  boolean active,
+                                  City city,
+                                  ClientSector clientSector,
+                                  String profilePicture,
+                                  Instant createdAt) {
 }
