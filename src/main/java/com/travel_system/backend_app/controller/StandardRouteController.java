9db4e3799e89f7142081dcd84d7a7b9aeff515c3
Iago-on-github <Iago737@outlook.com>
2026-08-10 17:12:38 -0300

    feat: add controller class to standardRoute

---
+++ src/main/java/com/travel_system/backend_app/controller/StandardRouteController.java
@@ -0,0 +1,102 @@
+package com.travel_system.backend_app.controller;
+
+import com.travel_system.backend_app.model.dtos.request.RouteStopReorderRequestDTO;
+import com.travel_system.backend_app.model.dtos.request.StandardRouteRequestDTO;
+import com.travel_system.backend_app.model.dtos.request.StandardRouteStopsUpdateDTO;
+import com.travel_system.backend_app.model.dtos.request.StandardRouteUpdateDTO;
+import com.travel_system.backend_app.model.dtos.response.StandardRouteResponseDTO;
+import com.travel_system.backend_app.model.enums.GeneralStatus;
+import com.travel_system.backend_app.service.StandardRouteService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/standard-route")
+public class StandardRouteController {
+    private final StandardRouteService standardRouteService;
+
+    public StandardRouteController(StandardRouteService standardRouteService) {
+        this.standardRouteService = standardRouteService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Page<StandardRouteResponseDTO>> getAllStandardRoutes() {
+        return ResponseEntity.ok().body(standardRouteService.getAllStandardRoutes());
+    }
+
+    @GetMapping("/{standardRouteId}")
+    public ResponseEntity<StandardRouteResponseDTO> getStandardRouteById(@PathVariable UUID standardRouteId) {
+        return ResponseEntity.ok().body(standardRouteService.getStandardRouteById(standardRouteId));
+    }
+
+    @GetMapping("/{customerId}/customer")
+    public ResponseEntity<Page<StandardRouteResponseDTO>> getAllStandardRouteByCustomer(@PathVariable UUID customerId) {
+        return ResponseEntity.ok().body(standardRouteService.getAllStandardRouteByCustomer(customerId));
+    }
+
+    @GetMapping("/{standardRouteId}/route-stops")
+    public ResponseEntity<StandardRouteResponseDTO> getStandardRouteStopPoints(@PathVariable UUID standardRouteId, @RequestParam GeneralStatus status) {
+        return ResponseEntity.ok().body(standardRouteService.getStandardRouteStopPoints(standardRouteId, status));
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<StandardRouteResponseDTO> createStandardRoute(Authentication auth, @RequestBody StandardRouteRequestDTO standardRouteRequestDTO, UriComponentsBuilder uriComponentsBuilder) {
+        String userAuthenticatedEmail = auth.getName();
+        StandardRouteResponseDTO newStandardRoute = standardRouteService.createStandardRoute(userAuthenticatedEmail, standardRouteRequestDTO);
+
+        URI uri = uriComponentsBuilder.path("/{id}").buildAndExpand(newStandardRoute.id()).toUri();
+
+        return ResponseEntity.created(uri).body(newStandardRoute);
+    }
+
+    @PatchMapping("/{standardRouteId}/update")
+    public ResponseEntity<StandardRouteResponseDTO> updateStandardRoute(Authentication auth, @PathVariable UUID standardRouteId, @RequestBody StandardRouteUpdateDTO standardRouteUpdateDTO) {
+        String authenticatedEmail = auth.getName();
+
+        return ResponseEntity.ok().body(standardRouteService.updateStandardRoute(standardRouteId, authenticatedEmail, standardRouteUpdateDTO));
+    }
+
+    @PatchMapping("/{standardRouteId}/update-route-stop")
+    public ResponseEntity<StandardRouteResponseDTO> updateRouteStopPoints(@PathVariable UUID standardRouteId, Authentication auth, @RequestBody StandardRouteStopsUpdateDTO standardRouteStopsUpdateDTO) {
+        String authenticatedEmail = auth.getName();
+
+        return ResponseEntity.ok().body(standardRouteService.updateRouteStopPoints(standardRouteId, authenticatedEmail, standardRouteStopsUpdateDTO));
+    }
+
+    @PatchMapping("/{standardRouteId}/route-stop-status")
+    public ResponseEntity<Void> updateRouteStopStatus(@PathVariable UUID standardRouteId, Authentication auth, @RequestParam GeneralStatus status) {
+        String authenticatedEmail = auth.getName();
+
+        standardRouteService.updateRouteStopStatus(standardRouteId, authenticatedEmail, status);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{standardRouteId}/associate/{routeStopId}")
+    public ResponseEntity<Void> associateRouteStopWithStandardRoute(@PathVariable UUID standardRouteId, @PathVariable UUID routeStopId, @RequestParam int sequence, @RequestParam boolean isOptionalSpot) {
+        standardRouteService.associateRouteStopWithStandardRoute(standardRouteId, routeStopId, sequence, isOptionalSpot);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{standardRouteId}/remove/{routeStopId}")
+    public ResponseEntity<Void> removeRouteStopWithStandardRoute(@PathVariable UUID standardRouteId, @PathVariable UUID routeStopId) {
+        standardRouteService.removeRouteStopWithStandardRoute(standardRouteId, routeStopId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{standardRouteId}/reorder/{routeStopId}")
+    public ResponseEntity<StandardRouteResponseDTO> reorderRouteStops(@PathVariable UUID standardRouteId, @RequestBody List<RouteStopReorderRequestDTO> routeStopsReorder) {
+        return ResponseEntity.ok().body(standardRouteService.reorderRouteStops(standardRouteId, routeStopsReorder));
+    }
+
+
+}
