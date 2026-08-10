283f39e34af88b3f9bf17f0673028692e98d2ebb
Iago-on-github <Iago737@outlook.com>
2026-08-10 17:07:01 -0300

    feat: add 'request' and 'response' DTO from mapped standardRoute entity.

---
+++ src/main/java/com/travel_system/backend_app/model/dtos/request/StandardRouteRequestDTO.java
@@ -0,0 +1,29 @@
+package com.travel_system.backend_app.model.dtos.request;
+
+import com.travel_system.backend_app.model.enums.TravelPeriod;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+public record StandardRouteRequestDTO(
+        @NotNull @NotBlank(message = "O nome da rota é obrigatório")
+        String routeName,
+        @NotBlank(message = "A descrição da rota é obrigatória")
+        String routeDescription,
+        @NotNull(message = "A latitude de origem é obrigatória")
+        Double originLatitude,
+        @NotNull(message = "A longitude de origem é obrigatória")
+        Double originLongitude,
+        @NotNull(message = "A latitude de destino é obrigatória")
+        Double destinationLatitude,
+        @NotNull(message = "A longitude de destino é obrigatória")
+        Double destinationLongitude,
+        @NotNull(message = "O período de viagem é obrigatório")
+        TravelPeriod period,
+        @NotEmpty(message = "A lista de paradas não pode ser nula nem vazia")
+        Set<@Valid @NotNull RouteStopAssignmentRequestDTO> routeStops
+) {
+}
---
+++ src/main/java/com/travel_system/backend_app/model/dtos/response/StandardRouteResponseDTO.java
@@ -0,0 +1,26 @@
+package com.travel_system.backend_app.model.dtos.response;
+
+import com.travel_system.backend_app.model.dtos.request.RouteStopAssignmentRequestDTO;
+import com.travel_system.backend_app.model.enums.GeneralStatus;
+import com.travel_system.backend_app.model.enums.TravelPeriod;
+
+import java.time.Instant;
+import java.util.Set;
+import java.util.UUID;
+
+public record StandardRouteResponseDTO(
+        UUID id,
+        String routeName,
+        String routeDescription,
+        Double originLatitude,
+        Double originLongitude,
+        Double destinationLatitude,
+        Double destinationLongitude,
+        String standardGeometry,
+        TravelPeriod travelPeriod,
+        Set<RouteStopAssignmentResponseDTO> routeStopAssignments,
+        UUID customerId,
+        GeneralStatus status,
+        Instant createdAt,
+        Instant updatedAt) {
+}
