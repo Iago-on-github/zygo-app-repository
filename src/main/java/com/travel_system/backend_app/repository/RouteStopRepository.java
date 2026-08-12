9acf82d8eaca80aba2ee5acac5e6ca51e2c726da
Iago-on-github <Iago737@outlook.com>
2026-08-12 16:44:38 -0300

    feat: add repository methods to find routeStop by parameter

--- src/main/java/com/travel_system/backend_app/repository/RouteStopRepository.java
+++ src/main/java/com/travel_system/backend_app/repository/RouteStopRepository.java
@@ -2,10 +2,21 @@ package com.travel_system.backend_app.repository;
 
 import com.travel_system.backend_app.model.RouteStop;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RouteStopRepository extends JpaRepository<RouteStop, UUID> {
+    @Query("SELECT rs FROM RouteStop rs WHERE rs.customer.id = :customerId")
+    List<RouteStop> findRouteStopsByCustomerId(@Param("customerId") UUID customerId);
+
+    Optional<RouteStop> findByName(String routeName);
+
+    boolean findByNameAndCustomerId(@NotNull String name, UUID customerId);
 }
