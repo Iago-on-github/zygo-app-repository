e1934bdc2bbb785636d5662ed46e0b11c83c04d7
Iago-on-github <Iago737@outlook.com>
2026-08-12 16:45:23 -0300

    feat: add request mapper for RouteStop

---
+++ src/main/java/com/travel_system/backend_app/interfaces/mappers/RouteStopRequestMapper.java
@@ -0,0 +1,24 @@
+package com.travel_system.backend_app.interfaces.mappers;
+
+import com.travel_system.backend_app.model.RouteStop;
+import com.travel_system.backend_app.model.dtos.request.RouteStopRequestDTO;
+import com.travel_system.backend_app.model.dtos.request.RouteStopUpdateDTO;
+import com.travel_system.backend_app.model.dtos.response.RouteStopResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface RouteStopRequestMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "customer", ignore = true)
+    @Mapping(target = "students", ignore = true)
+    RouteStop toEntity(RouteStopRequestDTO routeStopRequestDTO);
+
+    @Mapping(target = "latitude", ignore = true)
+    @Mapping(target = "longitude", ignore = true)
+    void routeStopUpdateDTO(RouteStopUpdateDTO routeStopUpdateDTO);
+}
