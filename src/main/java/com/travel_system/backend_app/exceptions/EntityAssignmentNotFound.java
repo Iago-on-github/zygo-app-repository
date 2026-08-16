e0a9bce0708872b8b245d06b217fe5bb64d9ec71
Iago-on-github <Iago737@outlook.com>
2026-08-16 20:47:47 -0300

    exception: add new exception for relation-entity not found

---
+++ src/main/java/com/travel_system/backend_app/exceptions/EntityAssignmentNotFound.java
@@ -0,0 +1,7 @@
+package com.travel_system.backend_app.exceptions;
+
+public class EntityAssignmentNotFound extends RuntimeException {
+    public EntityAssignmentNotFound(String message) {
+        super(message);
+    }
+}
