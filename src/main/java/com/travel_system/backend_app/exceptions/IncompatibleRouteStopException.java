f1ff0b52a068f8b9808740514e33e203e06156a9
Iago-on-github <Iago737@outlook.com>
2026-08-19 17:23:05 -0300

    exceptions: adição de nova exception de incompatibilidade de ponto de parada

---
+++ src/main/java/com/travel_system/backend_app/exceptions/IncompatibleRouteStopException.java
@@ -0,0 +1,7 @@
+package com.travel_system.backend_app.exceptions;
+
+public class IncompatibleRouteStopException extends RuntimeException {
+    public IncompatibleRouteStopException(String message) {
+        super(message);
+    }
+}
