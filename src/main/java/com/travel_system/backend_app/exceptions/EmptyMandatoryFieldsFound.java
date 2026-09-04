56cb472633fe8429d4543fb462eb541fed9749e5
Iago-on-github <Iago737@outlook.com>
2026-09-04 12:27:29 -0300

    remove/rename: removendo/renomeando classes MapStruct e Repositories

--- src/main/java/com/travel_system/backend_app/exceptions/EmptyMandatoryFieldsFound.java
+++
@@ -1,8 +0,0 @@
-package com.travel_system.backend_app.exceptions;
-
-public class EmptyMandatoryFieldsFound extends RuntimeException {
-
-    public EmptyMandatoryFieldsFound(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/travel_system/backend_app/exceptions/EntityAssignmentNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.travel_system.backend_app.exceptions;
-
-public class EntityAssignmentNotFound extends RuntimeException {
-    public EntityAssignmentNotFound(String message) {
-        super(message);
-    }
-}
