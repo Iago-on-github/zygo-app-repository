d6d590689c32f695db3b2bbb615b6cd44ba7a5bf
Iago-on-github <Iago737@outlook.com>
2026-09-14 16:50:39 -0300

    feat: add classe de constantes para os responsáveis

---
+++ src/main/java/com/travel_system/backend_app/config/constants/ResponsibleAdultConstants.java
@@ -0,0 +1,11 @@
+package com.travel_system.backend_app.config.constants;
+
+public class ResponsibleAdultConstants {
+
+    public ResponsibleAdultConstants() {
+        throw new UnsupportedOperationException("Não é possível instanciar uma classe de constantes");
+    }
+
+    // quantidade máxima de estudantes por adulto responsável
+    public static final int MAX_STUDENTS_PER_RESPONSIBLE_ADULT = 3;
+}
